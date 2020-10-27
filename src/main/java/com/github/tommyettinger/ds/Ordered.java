b66bb14cba762cc9005e79525723cf65b5f02125
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-10-26 19:36:00 -0700

    Add int, long, and float specializations for Ordered

--- src/main/java/com/github/tommyettinger/ds/Ordered.java
+++ src/main/java/com/github/tommyettinger/ds/Ordered.java
@@ -53,4 +53,117 @@ public interface Ordered<T> extends Arrangeable {
 	default void reverse () {
 		Collections.reverse(order());
 	}
+
+	/**
+	 * A primitive specialization of {@link Ordered} for collections of int values instead of objects.
+	 */
+	interface OfInt extends Arrangeable {
+		/**
+		 * Gets the IntList of int items that this data structure holds, in the order it uses for iteration.
+		 * This should usually return a direct reference to an IntList used inside this object, so changes
+		 * to the list will affect this.
+		 *
+		 * @return the IntList of int items that this data structure holds
+		 */
+		IntList order ();
+
+		/**
+		 * Switches the ordering of positions {@code a} and {@code b}, without changing any items beyond that.
+		 *
+		 * @param a the first position
+		 * @param b the second position
+		 */
+		@Override
+		default void swap (int a, int b) {
+			order().swap(a, b);
+		}
+
+		/**
+		 * Pseudo-randomly shuffles the order of this Ordered in-place.
+		 *
+		 * @param random any {@link Random} implementation; prefer {@link LaserRandom} in this library
+		 */
+		@Override
+		default void shuffle (Random random) {
+			IntList order = order();
+			for (int i = order.size() - 1; i >= 0; i--) {
+				order.swap(i, random.nextInt(i + 1));
+			}
+		}
+
+	}
+	/**
+	 * A primitive specialization of {@link Ordered} for collections of long values instead of objects.
+	 */
+	interface OfLong extends Arrangeable {
+		/**
+		 * Gets the LongList of long items that this data structure holds, in the order it uses for iteration.
+		 * This should usually return a direct reference to an LongList used inside this object, so changes
+		 * to the list will affect this.
+		 *
+		 * @return the LongList of long items that this data structure holds
+		 */
+		LongList order ();
+
+		/**
+		 * Switches the ordering of positions {@code a} and {@code b}, without changing any items beyond that.
+		 *
+		 * @param a the first position
+		 * @param b the second position
+		 */
+		@Override
+		default void swap (int a, int b) {
+			order().swap(a, b);
+		}
+
+		/**
+		 * Pseudo-randomly shuffles the order of this Ordered in-place.
+		 *
+		 * @param random any {@link Random} implementation; prefer {@link LaserRandom} in this library
+		 */
+		@Override
+		default void shuffle (Random random) {
+			LongList order = order();
+			for (int i = order.size() - 1; i >= 0; i--) {
+				order.swap(i, random.nextInt(i + 1));
+			}
+		}
+	}
+	/**
+	 * A primitive specialization of {@link Ordered} for collections of float values instead of objects.
+	 */
+	interface OfFloat extends Arrangeable {
+		/**
+		 * Gets the FloatList of float items that this data structure holds, in the order it uses for iteration.
+		 * This should usually return a direct reference to an FloatList used inside this object, so changes
+		 * to the list will affect this.
+		 *
+		 * @return the FloatList of float items that this data structure holds
+		 */
+		FloatList order ();
+
+		/**
+		 * Switches the ordering of positions {@code a} and {@code b}, without changing any items beyond that.
+		 *
+		 * @param a the first position
+		 * @param b the second position
+		 */
+		@Override
+		default void swap (int a, int b) {
+			order().swap(a, b);
+		}
+
+		/**
+		 * Pseudo-randomly shuffles the order of this Ordered in-place.
+		 *
+		 * @param random any {@link Random} implementation; prefer {@link LaserRandom} in this library
+		 */
+		@Override
+		default void shuffle (Random random) {
+			FloatList order = order();
+			for (int i = order.size() - 1; i >= 0; i--) {
+				order.swap(i, random.nextInt(i + 1));
+			}
+		}
+	}
 }
