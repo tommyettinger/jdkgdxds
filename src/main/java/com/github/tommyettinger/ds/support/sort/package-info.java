ce12d573b392c3aaa09bb9a98d4e10077b7717d7
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-10-27 22:48:20 -0700

    Start bringing in FastUtil's small primitive-comparison classes

---
+++ src/main/java/com/github/tommyettinger/ds/support/sort/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Code for customized sorting on primitive arrays and collections. This is Apache-licensed code from the
+ * <a href="https://github.com/vigna/fastutil">FastUtil project</a> with minimal changes.
+ */
+@NotNullDefault
+package com.github.tommyettinger.ds.support.sort;
+
+import com.github.tommyettinger.ds.annotations.NotNullDefault;
