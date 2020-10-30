a62b647740fd23a871d3b2d17300359f8bd91474
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-10-29 19:29:21 -0700

    ObjectFloatOrderedMap, so far this is easy...

---
+++ src/main/java/com/github/tommyettinger/ds/ObjectFloatOrderedMap.java
@@ -0,0 +1,524 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.github.tommyettinger.ds;
+
+import com.github.tommyettinger.ds.support.sort.FloatComparator;
+import com.github.tommyettinger.ds.support.sort.FloatComparators;
+import com.github.tommyettinger.ds.support.util.FloatIterator;
+
+import javax.annotation.Nullable;
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+import static com.github.tommyettinger.ds.Utilities.tableSize;
+
+/**
+ * An {@link ObjectFloatMap} that also stores keys in an {@link ObjectList} using the insertion order. Null keys are not allowed. No
+ * allocation is done except when growing the table size.
+ * <p>
+ * Iteration over the {@link #entrySet()} ()}, {@link #keySet()} ()}, and {@link #values()} is ordered and faster than an unordered map. Keys
+ * can also be accessed and the order changed using {@link #order()}. There is some additional overhead for put and remove.
+ * <p>
+ * This class performs fast contains (typically O(1), worst case O(n) but that is rare in practice). Remove is somewhat slower due
+ * to {@link #order()}. Add may be slightly slower, depending on hash collisions. Hashcodes are rehashed to reduce
+ * collisions and the need to resize. Load factors greater than 0.91 greatly increase the chances to resize to the next higher POT
+ * size.
+ * <p>
+ * Unordered sets and maps are not designed to provide especially fast iteration. Iteration is faster with {@link Ordered} types like
+ * ObjectOrderedSet and ObjectObjectOrderedMap.
+ * <p>
+ * You can customize most behavior of this map by extending it. {@link #place(Object)} can be overridden to change how hashCodes
+ * are calculated (which can be useful for types like {@link StringBuilder} that don't implement hashCode()), and
+ * {@link #locateKey(Object)} can be overridden to change how equality is calculated.
+ * <p>
+ * This implementation uses linear probing with the backward shift algorithm for removal. Hashcodes are rehashed using Fibonacci
+ * hashing, instead of the more common power-of-two mask, to better distribute poor hashCodes (see <a href=
+ * "https://probablydance.com/2018/06/16/fibonacci-hashing-the-optimization-that-the-world-forgot-or-a-better-alternative-to-integer-modulo/">Malte
+ * Skarupke's blog post</a>). Linear probing continues to work even when all hashCodes collide, just more slowly.
+ *
+ * @author Nathan Sweet
+ * @author Tommy Ettinger
+ */
+public class ObjectFloatOrderedMap<K> extends ObjectFloatMap<K> implements Ordered<K>, Serializable {
+	private static final long serialVersionUID = 0L;
+
+	protected final ObjectList<K> keys;
+
+	public ObjectFloatOrderedMap () {
+		keys = new ObjectList<>();
+	}
+
+	public ObjectFloatOrderedMap (int initialCapacity) {
+		super(initialCapacity);
+		keys = new ObjectList<>(initialCapacity);
+	}
+
+	public ObjectFloatOrderedMap (int initialCapacity, float loadFactor) {
+		super(initialCapacity, loadFactor);
+		keys = new ObjectList<>(initialCapacity);
+	}
+
+	public ObjectFloatOrderedMap (ObjectFloatOrderedMap<? extends K> map) {
+		super(map);
+		keys = new ObjectList<>(map.keys);
+	}
+
+	/**
+	 * Creates a new map identical to the specified map.
+	 */
+	public ObjectFloatOrderedMap (ObjectFloatMap<? extends K> map) {
+		this(map.size());
+		for (K k : map.keySet()) {
+			put(k, map.get(k));
+		}
+	}
+
+	@Override
+	public float put (K key, float value) {
+		int i = locateKey(key);
+		if (i >= 0) { // Existing key was found.
+			float oldValue = valueTable[i];
+			valueTable[i] = value;
+			return oldValue;
+		}
+		i = ~i; // Empty space was found.
+		keyTable[i] = key;
+		valueTable[i] = value;
+		keys.add(key);
+		if (++size >= threshold) { resize(keyTable.length << 1); }
+		return defaultValue;
+	}
+
+	public <T extends K> void putAll (ObjectFloatOrderedMap<T> map) {
+		ensureCapacity(map.size);
+		ObjectList<T> ks = map.keys;
+		int kl = ks.size();
+		T k;
+		for (int i = 0; i < kl; i++) {
+			k = ks.get(i);
+			put(k, map.get(k));
+		}
+	}
+
+	@Override
+	public float remove (Object key) {
+		if (!keys.remove(key)) { return defaultValue; }
+		return super.remove(key);
+	}
+
+	@Nullable
+	public float removeIndex (int index) {
+		return super.remove(keys.remove(index));
+	}
+
+	/**
+	 * Increases the size of the backing array to accommodate the specified number of additional items / loadFactor. Useful before
+	 * adding many items to avoid multiple backing array resizes.
+	 *
+	 * @param additionalCapacity how many additional items this should be able to hold without resizing (probably)
+	 */
+	@Override
+	public void ensureCapacity (int additionalCapacity) {
+		int tableSize = tableSize(size + additionalCapacity, loadFactor);
+		if (keyTable.length < tableSize) { resize(tableSize); }
+		keys.ensureCapacity(size + additionalCapacity);
+
+	}
+
+	/**
+	 * Changes the key {@code before} to {@code after} without changing its position in the order or its value. Returns true if
+	 * {@code after} has been added to the ObjectFloatOrderedMap and {@code before} has been removed; returns false if {@code after} is
+	 * already present or {@code before} is not present. If you are iterating over an ObjectFloatOrderedMap and have an index, you should
+	 * prefer {@link #alterIndex(int, Object)}, which doesn't need to search for an index like this does and so can be faster.
+	 *
+	 * @param before a key that must be present for this to succeed
+	 * @param after  a key that must not be in this map for this to succeed
+	 * @return true if {@code before} was removed and {@code after} was added, false otherwise
+	 */
+	public boolean alter (K before, K after) {
+		if (containsKey(after)) { return false; }
+		int index = keys.indexOf(before);
+		if (index == -1) { return false; }
+		super.put(after, super.remove(before));
+		keys.set(index, after);
+		return true;
+	}
+
+	/**
+	 * Changes the key at the given {@code index} in the order to {@code after}, without changing the ordering of other entries or
+	 * any values. If {@code after} is already present, this returns false; it will also return false if {@code index} is invalid
+	 * for the size of this map. Otherwise, it returns true. Unlike {@link #alter(Object, Object)}, this operates in constant time.
+	 *
+	 * @param index the index in the order of the key to change; must be non-negative and less than {@link #size}
+	 * @param after the key that will replace the contents at {@code index}; this key must not be present for this to succeed
+	 * @return true if {@code after} successfully replaced the key at {@code index}, false otherwise
+	 */
+	public boolean alterIndex (int index, K after) {
+		if (index < 0 || index >= size || containsKey(after)) { return false; }
+		super.put(after, super.remove(keys.get(index)));
+		keys.set(index, after);
+		return true;
+	}
+
+	/**
+	 * Changes the value at a specified {@code index} in the iteration order to {@code v}, without changing keys at all.
+	 * If {@code index} isn't currently a valid index in the iteration order, this returns null. Otherwise, it returns the
+	 * value that was previously held at {@code index}, which may also be null.
+	 *
+	 * @param v     the new float value to assign
+	 * @param index the index in the iteration order to set {@code v} at
+	 * @return the previous value held at {@code index} in the iteration order, which may be null if the value was null or if {@code index} was invalid
+	 */
+	@Nullable
+	public float setIndex (int index, float v) {
+		if (index < 0 || index >= size) { return defaultValue; }
+		final int pos = locateKey(keys.get(index));
+		final float oldValue = valueTable[pos];
+		valueTable[pos] = v;
+		return oldValue;
+	}
+
+	@Override
+	public void clear (int maximumCapacity) {
+		keys.clear();
+		super.clear(maximumCapacity);
+	}
+
+	@Override
+	public void clear () {
+		keys.clear();
+		super.clear();
+	}
+
+	/**
+	 * Gets the ObjectList of keys in the order this class will iterate through them.
+	 * Returns a direct reference to the same ObjectList this uses, so changes to the returned list will
+	 * also change the iteration order here.
+	 *
+	 * @return the ObjectList of keys, in iteration order (usually insertion-order), that this uses
+	 */
+	@Override
+	public ObjectList<K> order () {
+		return keys;
+	}
+
+	/**
+	 * Sorts this ObjectFloatOrderedMap in-place by the keys' natural ordering; {@code K} must implement {@link Comparable}.
+	 */
+	public void sort () {
+		keys.sort(null);
+	}
+
+	/**
+	 * Sorts this ObjectFloatOrderedMap in-place by the given Comparator used on the keys. If {@code comp} is null, then this
+	 * will sort by the natural ordering of the keys, which requires {@code K} to {@link Comparable}.
+	 *
+	 * @param comp a Comparator that can compare two {@code K} keys, or null to use the keys' natural ordering
+	 */
+	public void sort (@Nullable Comparator<K> comp) {
+		keys.sort(comp);
+	}
+
+	/**
+	 * Sorts this ObjectFloatOrderedMap in-place by the given FloatComparator used on the values. {@code comp} must not be null,
+	 * and must be able to compare {@code float} values. You can use {@link FloatComparators#NATURAL_COMPARATOR} to do
+	 * what {@link #sort()} does (just sorting values in this case instead of keys); there is also a reversed comparator
+	 * available, {@link FloatComparators#OPPOSITE_COMPARATOR}.
+	 *
+	 * @param comp a non-null {@link FloatComparator}
+	 */
+	public void sortByValue (FloatComparator comp) {
+		keys.sort((a, b) -> comp.compare(get(a), get(b)));
+	}
+
+	/**
+	 * Returns a {@link Set} view of the keys contained in this map.
+	 * The set is backed by the map, so changes to the map are
+	 * reflected in the set, and vice-versa.  If the map is modified
+	 * while an iteration over the set is in progress (except through
+	 * the iterator's own {@code remove} operation), the results of
+	 * the iteration are undefined.  The set supports element removal,
+	 * which removes the corresponding mapping from the map, via the
+	 * {@code Iterator.remove}, {@code Set.remove},
+	 * {@code removeAll}, {@code retainAll}, and {@code clear}
+	 * operations.  It does not support the {@code add} or {@code addAll}
+	 * operations.
+	 *
+	 * <p>Note that the same Collection instance is returned each time this
+	 * method is called. Use the {@link OrderedMapKeys#OrderedMapKeys(ObjectFloatOrderedMap)}
+	 * constructor for nested or multithreaded iteration.
+	 *
+	 * @return a set view of the keys contained in this map
+	 */
+	@Override
+	public Keys<K> keySet () {
+		if (keys1 == null || keys2 == null) {
+			keys1 = new OrderedMapKeys<>(this);
+			keys2 = new OrderedMapKeys<>(this);
+		}
+		if (!keys1.iter.valid) {
+			keys1.iter.reset();
+			keys1.iter.valid = true;
+			keys2.iter.valid = false;
+			return keys1;
+		}
+		keys2.iter.reset();
+		keys2.iter.valid = true;
+		keys1.iter.valid = false;
+		return keys2;
+	}
+
+	/**
+	 * Returns a Collection for the values in the map. Remove is supported by the Collection's iterator.
+	 * <p>Note that the same Collection instance is returned each time this method is called. Use the
+	 * {@link OrderedMapValues#OrderedMapValues(ObjectFloatOrderedMap)} constructor for nested or multithreaded iteration.
+	 *
+	 * @return a {@link PrimitiveCollection.OfFloat} of the float values
+	 */
+	@Override
+	public Values<K> values () {
+		if (values1 == null || values2 == null) {
+			values1 = new OrderedMapValues<>(this);
+			values2 = new OrderedMapValues<>(this);
+		}
+		if (!values1.iter.valid) {
+			values1.iter.reset();
+			values1.iter.valid = true;
+			values2.iter.valid = false;
+			return values1;
+		}
+		values2.iter.reset();
+		values2.iter.valid = true;
+		values1.iter.valid = false;
+		return values2;
+	}
+
+	/**
+	 * Returns a Set of Map.Entry, containing the entries in the map. Remove is supported by the Set's iterator.
+	 *
+	 * <p>Note that the same iterator instance is returned each time this method is called.
+	 * Use the {@link OrderedMapEntries#OrderedMapEntries(ObjectFloatOrderedMap)} constructor for nested or multithreaded iteration.
+	 *
+	 * @return a {@link Set} of {@link Map.Entry} key-value pairs
+	 */
+	@Override
+	public Entries<K> entrySet () {
+		if (entries1 == null || entries2 == null) {
+			entries1 = new OrderedMapEntries<>(this);
+			entries2 = new OrderedMapEntries<>(this);
+		}
+		if (!entries1.iter.valid) {
+			entries1.iter.reset();
+			entries1.iter.valid = true;
+			entries2.iter.valid = false;
+			return entries1;
+		}
+		entries2.iter.reset();
+		entries2.iter.valid = true;
+		entries1.iter.valid = false;
+		return entries2;
+	}
+
+	/**
+	 * Reuses the iterator of the reused {@link Entries}
+	 * produced by {@link #entrySet()}; does not permit nested iteration. Iterate over
+	 * {@link OrderedMapEntries#OrderedMapEntries(ObjectFloatOrderedMap)} if you need nested or
+	 * multithreaded iteration. You can remove an Entry from this ObjectFloatOrderedMap
+	 * using this Iterator.
+	 *
+	 * @return an {@link Iterator} over key-value pairs as {@link Map.Entry} values
+	 */
+	@Override
+	public Iterator<Entry<K>> iterator () {
+		return entrySet().iterator();
+	}
+
+	@Override
+	protected String toString (String separator, boolean braces) {
+		if (size == 0) { return braces ? "{}" : ""; }
+		StringBuilder buffer = new StringBuilder(32);
+		if (braces) { buffer.append('{'); }
+		ObjectList<K> keys = this.keys;
+		for (int i = 0, n = keys.size(); i < n; i++) {
+			K key = keys.get(i);
+			if (i > 0) { buffer.append(separator); }
+			buffer.append(key == this ? "(this)" : key);
+			buffer.append('=');
+			float value = get(key);
+			buffer.append(value);
+		}
+		if (braces) { buffer.append('}'); }
+		return buffer.toString();
+	}
+
+	public static class OrderedMapEntries<K> extends Entries<K> {
+		protected ObjectList<K> keys;
+
+		public OrderedMapEntries (ObjectFloatOrderedMap<K> map) {
+			super(map);
+			keys = map.keys;
+			iter = new EntryIterator<K>(map) {
+				public Iterator<Entry<K>> iterator () {
+					return this;
+				}
+
+				@Override
+				public void reset () {
+					currentIndex = -1;
+					nextIndex = 0;
+					hasNext = map.size > 0;
+				}
+
+				@Override
+				public boolean hasNext () {
+					if (!valid) { throw new RuntimeException("#iterator() cannot be used nested."); }
+					return hasNext;
+				}
+
+				@Override
+				public Entry<K> next () {
+					if (!hasNext) { throw new NoSuchElementException(); }
+					if (!valid) { throw new RuntimeException("#iterator() cannot be used nested."); }
+					currentIndex = nextIndex;
+					entry.key = keys.get(nextIndex);
+					entry.value = map.get(entry.key);
+					nextIndex++;
+					hasNext = nextIndex < map.size;
+					return entry;
+				}
+
+				@Override
+				public void remove () {
+					if (currentIndex < 0) { throw new IllegalStateException("next must be called before remove."); }
+					if(entry.key != null) {
+						map.remove(entry.key);
+					}
+					nextIndex--;
+					currentIndex = -1;
+				}
+			};
+		}
+
+		@Override
+		public Iterator<Entry<K>> iterator () {
+			return iter;
+		}
+	}
+
+	public static class OrderedMapKeys<K> extends Keys<K> {
+		private final ObjectList<K> keys;
+
+		public OrderedMapKeys (ObjectFloatOrderedMap<K> map) {
+			super(map);
+			keys = map.keys;
+			iter = new KeyIterator<K>(map) {
+				@Override
+				public Iterator<K> iterator () {
+					return this;
+				}
+
+				@Override
+				public boolean hasNext () {
+					if (!valid) { throw new RuntimeException("#iterator() cannot be used nested."); }
+					return hasNext;
+				}
+
+				@Override
+				public void reset () {
+					currentIndex = -1;
+					nextIndex = 0;
+					hasNext = map.size > 0;
+				}
+
+				@Override
+				public K next () {
+					if (!hasNext) { throw new NoSuchElementException(); }
+					if (!valid) { throw new RuntimeException("#iterator() cannot be used nested."); }
+					K key = keys.get(nextIndex);
+					currentIndex = nextIndex;
+					nextIndex++;
+					hasNext = nextIndex < map.size;
+					return key;
+				}
+
+				@Override
+				public void remove () {
+					if (currentIndex < 0) { throw new IllegalStateException("next must be called before remove."); }
+					map.remove(keys.get(currentIndex));
+					nextIndex = currentIndex;
+					currentIndex = -1;
+				}
+			};
+		}
+
+		@Override
+		public Iterator<K> iterator () {
+			return iter;
+		}
+	}
+
+	public static class OrderedMapValues<K> extends Values<K> {
+		private final ObjectList<K> keys;
+
+		public OrderedMapValues (ObjectFloatOrderedMap<K> map) {
+			super(map);
+			keys = map.keys;
+			iter = new ValueIterator<K>(map) {
+				@Override
+				public boolean hasNext () {
+					if (!valid) { throw new RuntimeException("#iterator() cannot be used nested."); }
+					return hasNext;
+				}
+
+				@Override
+				public void reset () {
+					currentIndex = -1;
+					nextIndex = 0;
+					hasNext = map.size > 0;
+				}
+
+				@Override
+				public float nextFloat () {
+					if (!hasNext) { throw new NoSuchElementException(); }
+					if (!valid) { throw new RuntimeException("#iterator() cannot be used nested."); }
+					float value = map.get(keys.get(nextIndex));
+					currentIndex = nextIndex;
+					nextIndex++;
+					hasNext = nextIndex < map.size;
+					return value;
+				}
+
+				@Override
+				public void remove () {
+					if (currentIndex < 0) { throw new IllegalStateException("next must be called before remove."); }
+					map.remove(keys.get(currentIndex));
+					nextIndex = currentIndex;
+					currentIndex = -1;
+				}
+			};
+		}
+
+		@Override
+		public FloatIterator iterator () {
+			return iter;
+		}
+	}
+}
