b1dda1db808aa7316b5e906f9342ba966fb7f224
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-12-02 02:41:02 -0800

    Evaluate possible problem relating to libgdx's 6285
    This project does fine with IntObjectMap.containsValue() and with IntIntMap.containsValue(); I'm pretty sure libGDX's code is similar.

---
+++ src/test/java/com/github/tommyettinger/ds/test/Issue6285Test.java
@@ -0,0 +1,33 @@
+package com.github.tommyettinger.ds.test;
+
+import com.github.tommyettinger.ds.HolderSet;
+import com.github.tommyettinger.ds.IntIntMap;
+import com.github.tommyettinger.ds.IntObjectMap;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Tests for <a href="https://github.com/libgdx/libgdx/issues/6285">libGDX issue #6285</a>.
+ */
+public class Issue6285Test {
+	@Test
+	public void testIntObjectMap6285 () {
+		IntObjectMap<Integer> map = new IntObjectMap<>();
+		Integer value = 123;
+		map.put(1, value);
+		map.remove(1);
+
+		Assert.assertFalse( map.containsValue(value) );
+	}
+	@Test
+	public void testIntIntMap6285 () {
+		IntIntMap map = new IntIntMap();
+		int value = 123;
+		map.put(1, value);
+		map.remove(1);
+
+		Assert.assertFalse( map.containsValue(value) );
+	}
+}
