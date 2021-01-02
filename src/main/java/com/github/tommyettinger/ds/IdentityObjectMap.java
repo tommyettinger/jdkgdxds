d924c6097b7ece6a25dfb3df9146898a782e9636
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-01-01 20:59:44 -0800

    Add draft of IdentityObjectMap.
    It may be usable in this form, it just isn't very much code, so I'm surprised.

---
+++ src/main/java/com/github/tommyettinger/ds/IdentityObjectMap.java
@@ -0,0 +1,139 @@
+package com.github.tommyettinger.ds;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * A variant on {@link ObjectObjectMap} that compares keys by identity (using {@code ==}) instead of equality (using {@code equals()}).
+ * It also hashes with {@link System#identityHashCode(Object)} instead of calling the {@code hashCode()} of an item.
+ * Note that the {@link #entrySet()}, {@link #keySet()} and individual Entry items this produces are those of an {@link ObjectObjectMap},
+ * and so do not compare by identity.
+ */
+public class IdentityObjectMap<K, V> extends ObjectObjectMap<K, V> {
+	/**
+	 * Creates a new map with an initial capacity of 51 and a load factor of 0.8.
+	 */
+	public IdentityObjectMap () {
+		super();
+	}
+
+	/**
+	 * Creates a new map with a load factor of 0.8.
+	 *
+	 * @param initialCapacity If not a power of two, it is increased to the next nearest power of two.
+	 */
+	public IdentityObjectMap (int initialCapacity) {
+		super(initialCapacity);
+	}
+
+	/**
+	 * Creates a new map with the specified initial capacity and load factor. This map will hold initialCapacity items before
+	 * growing the backing table.
+	 *
+	 * @param initialCapacity If not a power of two, it is increased to the next nearest power of two.
+	 * @param loadFactor
+	 */
+	public IdentityObjectMap (int initialCapacity, float loadFactor) {
+		super(initialCapacity, loadFactor);
+	}
+
+	/**
+	 * Creates a new map identical to the specified map.
+	 *
+	 * @param map
+	 */
+	public IdentityObjectMap (ObjectObjectMap<? extends K, ? extends V> map) {
+		super(map);
+	}
+
+	/**
+	 * Creates a new map identical to the specified map.
+	 *
+	 * @param map
+	 */
+	public IdentityObjectMap (Map<? extends K, ? extends V> map) {
+		super(map);
+	}
+
+	/**
+	 * Given two side-by-side arrays, one of keys, one of values, this constructs a map and inserts each pair of key and value into it.
+	 * If keys and values have different lengths, this only uses the length of the smaller array.
+	 *
+	 * @param keys   an array of keys
+	 * @param values an array of values
+	 */
+	public IdentityObjectMap (K[] keys, V[] values) {
+		super(keys, values);
+	}
+
+	/**
+	 * Given two side-by-side collections, one of keys, one of values, this constructs a map and inserts each pair of key and value into it.
+	 * If keys and values have different lengths, this only uses the length of the smaller collection.
+	 *
+	 * @param keys   a Collection of keys
+	 * @param values a Collection of values
+	 */
+	public IdentityObjectMap (Collection<? extends K> keys, Collection<? extends V> values) {
+		super(keys, values);
+	}
+
+	/**
+	 * Returns an index &gt;= 0 and &lt;= {@link #mask} for the specified {@code item}.
+	 * <p>
+	 * The default behavior uses Fibonacci hashing; it simply gets the {@link System#identityHashCode(Object)}
+	 * of {@code item}, multiplies it by a specific long constant related to the golden ratio,
+	 * and makes an unsigned right shift by {@link #shift} before casting to int and returning.
+	 * This can be overridden to hash {@code item} differently, though all implementors must
+	 * ensure this returns results in the range of 0 to {@link #mask}, inclusive. If nothing
+	 * else is changed, then unsigned-right-shifting an int or long by {@link #shift} will also
+	 * restrict results to the correct range.
+	 *
+	 * @param item a non-null Object; its identityHashCode is used here
+	 */
+	@Override
+	protected int place (Object item) {
+		return (int)(System.identityHashCode(item) * 0x9E3779B97F4A7C15L >>> shift);
+	}
+
+	/**
+	 * Returns the index of the key if already present, else {@code ~index} for the next empty index. This can be overridden
+	 * to compare for equality differently than by using {@code ==}, as this does.
+	 *
+	 * @param key a non-null Object that should probably be a K
+	 */
+	@Override
+	protected int locateKey (Object key) {
+		K[] keyTable = this.keyTable;
+		for (int i = place(key); ; i = i + 1 & mask) {
+			K other = keyTable[i];
+			if (other == null) {
+				return ~i; // Always negative; means empty space is available at i.
+			}
+			if (other == key) // If you want to change how equality is determined, do it here.
+			{
+				return i; // Same key was found.
+			}
+		}
+	}
+
+	@Override
+	public boolean equals (Object obj) {
+		return super.equalsIdentity(obj);
+	}
+	@Override
+	public int hashCode () {
+		int h = size;
+		K[] keyTable = this.keyTable;
+		V[] valueTable = this.valueTable;
+		for (int i = 0, n = keyTable.length; i < n; i++) {
+			K key = keyTable[i];
+			if (key != null) {
+				h ^= System.identityHashCode(key);
+				V value = valueTable[i];
+				if (value != null) { h ^= value.hashCode(); }
+			}
+		}
+		return h;
+	}
+
+}
