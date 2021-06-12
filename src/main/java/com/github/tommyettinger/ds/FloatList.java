de435b7b488aaab49bbaff657abb7f5ded06cae2
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-06-11 20:36:18 -0700

    New sorting now in FloatList.

--- src/main/java/com/github/tommyettinger/ds/FloatList.java
+++ src/main/java/com/github/tommyettinger/ds/FloatList.java
@@ -19,6 +19,9 @@ package com.github.tommyettinger.ds;
 import com.github.tommyettinger.ds.support.BitConversion;
 import com.github.tommyettinger.ds.support.function.FloatUnaryOperator;
 import com.github.tommyettinger.ds.support.sort.FloatComparator;
+import com.github.tommyettinger.ds.support.sort.FloatComparators;
+import com.github.tommyettinger.ds.support.sort.LongComparator;
+import com.github.tommyettinger.ds.support.sort.LongComparators;
 import com.github.tommyettinger.ds.support.util.FloatIterator;
 
 import javax.annotation.Nullable;
@@ -835,111 +838,10 @@ public class FloatList implements PrimitiveCollection.OfFloat, Ordered.OfFloat, 
 		return new FloatList(array);
 	}
 
-/// The remainder of the code is based on FastUtil.
-
-	/**
-	 * Transforms two consecutive sorted ranges into a single sorted range. The initial ranges are
-	 * {@code [first..middle)} and {@code [middle..last)}, and the resulting range is
-	 * {@code [first..last)}. Elements in the first input range will precede equal elements in
-	 * the second.
-	 */
-	private void inPlaceMerge (final int from, int mid, final int to, final FloatComparator comp) {
-		if (from >= mid || mid >= to) { return; }
-		if (to - from == 2) {
-			if (comp.compare(get(mid), get(from)) < 0) { swap(from, mid); }
-			return;
-		}
-
-		int firstCut;
-		int secondCut;
-
-		if (mid - from > to - mid) {
-			firstCut = from + (mid - from) / 2;
-			secondCut = lowerBound(mid, to, firstCut, comp);
-		} else {
-			secondCut = mid + (to - mid) / 2;
-			firstCut = upperBound(from, mid, secondCut, comp);
-		}
-
-		int first2 = firstCut;
-		int middle2 = mid;
-		int last2 = secondCut;
-		if (middle2 != first2 && middle2 != last2) {
-			int first1 = first2;
-			int last1 = middle2;
-			while (first1 < --last1) { swap(first1++, last1); }
-			first1 = middle2;
-			last1 = last2;
-			while (first1 < --last1) { swap(first1++, last1); }
-			first1 = first2;
-			last1 = last2;
-			while (first1 < --last1) { swap(first1++, last1); }
-		}
-
-		mid = firstCut + secondCut - mid;
-		inPlaceMerge(from, firstCut, mid, comp);
-		inPlaceMerge(mid, secondCut, to, comp);
-	}
-
-	/**
-	 * Performs a binary search on an already-sorted range: finds the first position where an
-	 * element can be inserted without violating the ordering. Sorting is by a user-supplied
-	 * comparison function.
-	 *
-	 * @param from the index of the first element (inclusive) to be included in the binary search.
-	 * @param to   the index of the last element (exclusive) to be included in the binary search.
-	 * @param pos  the position of the element to be searched for.
-	 * @param comp the comparison function.
-	 * @return the largest index i such that, for every j in the range {@code [first..i)},
-	 * {@code comp.compare(get(j), get(pos))} is {@code true}.
-	 */
-	private int lowerBound (int from, final int to, final int pos, final FloatComparator comp) {
-		int len = to - from;
-		float[] items = this.items;
-		while (len > 0) {
-			int half = len / 2;
-			int middle = from + half;
-			if (comp.compare(items[middle], items[pos]) < 0) {
-				from = middle + 1;
-				len -= half + 1;
-			} else {
-				len = half;
-			}
-		}
-		return from;
-	}
-
-	/**
-	 * Performs a binary search on an already sorted range: finds the last position where an element
-	 * can be inserted without violating the ordering. Sorting is by a user-supplied comparison
-	 * function.
-	 *
-	 * @param from the index of the first element (inclusive) to be included in the binary search.
-	 * @param to   the index of the last element (exclusive) to be included in the binary search.
-	 * @param pos  the position of the element to be searched for.
-	 * @param comp the comparison function.
-	 * @return The largest index i such that, for every j in the range {@code [first..i)},
-	 * {@code comp.compare(get(pos), get(j))} is {@code false}.
-	 */
-	private int upperBound (int from, final int to, final int pos, final FloatComparator comp) {
-		int len = to - from;
-		float[] items = this.items;
-		while (len > 0) {
-			int half = len / 2;
-			int middle = from + half;
-			if (comp.compare(items[pos], items[middle]) < 0) {
-				len = half;
-			} else {
-				from = middle + 1;
-				len -= half + 1;
-			}
-		}
-		return from;
-	}
-
 	/**
 	 * Sorts all elements according to the order induced by the specified
-	 * comparator using mergesort. If {@code c} is null, this instead delegates to {@link #sort()},
+	 * comparator using {@link FloatComparators#sort(float[], int, int, FloatComparator)}.
+	 * If {@code c} is null, this instead delegates to {@link #sort()},
 	 * which uses {@link Arrays#sort(float[])}, and does not always run in-place.
 	 *
 	 * <p>This sort is guaranteed to be <i>stable</i>: equal elements will not be reordered as a result
@@ -959,55 +861,15 @@ public class FloatList implements PrimitiveCollection.OfFloat, Ordered.OfFloat, 
 
 	/**
 	 * Sorts the specified range of elements according to the order induced by the specified
-	 * comparator using mergesort.
-	 *
-	 * <p>This sort is guaranteed to be <i>stable</i>: equal elements will not be reordered as a result
-	 * of the sort. The sorting algorithm is an in-place mergesort that is significantly slower than a
-	 * standard mergesort, as its running time is <i>O</i>(<var>n</var>&nbsp;(log&nbsp;<var>n</var>)<sup>2</sup>), but it does not allocate additional memory; as a result, it can be
-	 * used as a generic sorting algorithm.
+	 * comparator using mergesort, or {@link Arrays#sort(float[], int, int)} if {@code c} is null.
+	 * This purely uses {@link FloatComparators#sort(float[], int, int, FloatComparator)}, and you
+	 * can see its docs for more information.
 	 *
 	 * @param from the index of the first element (inclusive) to be sorted.
 	 * @param to   the index of the last element (exclusive) to be sorted.
 	 * @param c    the comparator to determine the order of the FloatList
 	 */
 	public void sort (final int from, final int to, final FloatComparator c) {
-		if (to <= 0) {
-			return;
-		}
-		if (from < 0 || from >= size || to > size) {
-			throw new UnsupportedOperationException("The given from/to range in FloatList.sort() is invalid.");
-		}
-		/*
-		 * We retain the same method signature as quickSort. Given only a comparator and this list
-		 * do not know how to copy and move elements from/to temporary arrays. Hence, in contrast to
-		 * the JDK mergesorts this is an "in-place" mergesort, i.e. does not allocate any temporary
-		 * arrays. A non-inplace mergesort would perhaps be faster in most cases, but would require
-		 * non-intuitive delegate objects...
-		 */
-		final int length = to - from;
-
-		float[] items = this.items;
-
-		// Insertion sort on smallest arrays, less than 16 items
-		if (length < 16) {
-			for (int i = from; i < to; i++) {
-				for (int j = i; j > from && c.compare(items[j - 1], items[j]) > 0; j--) {
-					swap(j, j - 1);
-				}
-			}
-			return;
-		}
-
-		// Recursively sort halves
-		int mid = from + to >>> 1;
-		sort(from, mid, c);
-		sort(mid, to, c);
-
-		// If list is already sorted, nothing left to do. This is an
-		// optimization that results in faster sorts for nearly ordered lists.
-		if (c.compare(items[mid - 1], items[mid]) <= 0) { return; }
-
-		// Merge sorted halves
-		inPlaceMerge(from, mid, to, c);
+		FloatComparators.sort(items, from, to, c);
 	}
 }
