257d096cb5e36913b37cbef53f2d4fb3c7c54193
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-06-12 19:26:15 -0700

    DoubleDeque sorting.

--- src/main/java/com/github/tommyettinger/ds/DoubleDeque.java
+++ src/main/java/com/github/tommyettinger/ds/DoubleDeque.java
@@ -18,6 +18,8 @@ package com.github.tommyettinger.ds;
 
 import com.github.tommyettinger.ds.support.BitConversion;
 import com.github.tommyettinger.ds.support.EnhancedRandom;
+import com.github.tommyettinger.ds.support.sort.DoubleComparator;
+import com.github.tommyettinger.ds.support.sort.DoubleComparators;
 
 import javax.annotation.Nullable;
 import java.util.Arrays;
@@ -971,8 +973,26 @@ public class DoubleDeque implements PrimitiveCollection.OfDouble, Arrangeable {
 			Arrays.sort(values, head, tail);
 		} else {
 			System.arraycopy(values, head, values, tail, values.length - head);
-			Arrays.sort(values, 0, tail + values.length - head);
-			tail = tail + values.length - head;
+			Arrays.sort(values, 0, size);
+			tail = size;
+			head = 0;
+		}
+	}
+
+	/**
+	 * Sorts this deque in-place using {@link DoubleComparators#sort(double[], int, int, DoubleComparator)}.
+	 * This should operate in O(n log(n)) time or less when the internals of the deque are
+	 * continuous (the head is before the tail in the array). If the internals are not
+	 * continuous, this takes an additional O(n) step (where n is less than the size of
+	 * the deque) to rearrange the internals before sorting.
+	 */
+	public void sort(@Nullable DoubleComparator c){
+		if(head <= tail) {
+			DoubleComparators.sort(values, head, tail, c);
+		} else {
+			System.arraycopy(values, head, values, tail, values.length - head);
+			DoubleComparators.sort(values, 0, size, c);
+			tail = size;
 			head = 0;
 		}
 	}
