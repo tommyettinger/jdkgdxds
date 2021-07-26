b546cb6fa2fee693f9dc78f9460a95173247039e
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-07-25 22:21:19 -0700

    Big license cleanup!
    This adds the AUTHORS, CONTRIBUTORS, and THANKS files, the last one because I do want to thank people who have indirectly contributed. I also updated the copyright to 2021 for all files; almost everything was added this year, and if it wasn't, it was probably heavily updated this year. The Apache license header is now on every source file.

--- src/main/java/com/github/tommyettinger/ds/support/sort/IntComparator.java
+++ src/main/java/com/github/tommyettinger/ds/support/sort/IntComparator.java
@@ -22,11 +22,6 @@ import java.util.Comparator;
  * A type-specific {@link Comparator}; provides methods to compare two primitive
  * types both as objects and as primitive types.
  *
- * <p>
- * Note that {@code fastutil} provides a corresponding abstract class that can
- * be used to implement this interface just by specifying the type-specific
- * comparator.
- *
  * @see Comparator
  * @see IntComparators
  */
