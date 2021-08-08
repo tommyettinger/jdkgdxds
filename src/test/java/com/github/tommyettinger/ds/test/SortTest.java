154e9dc9cccd83c9469e29551b34d4709461be69
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-08-08 02:08:17 -0700

    The sort works!

--- src/test/java/com/github/tommyettinger/ds/test/SortTest.java
+++ src/test/java/com/github/tommyettinger/ds/test/SortTest.java
@@ -1,6 +1,7 @@
 package com.github.tommyettinger.ds.test;
 
 import com.github.tommyettinger.ds.ObjectList;
+import com.github.tommyettinger.ds.support.sort.NaturalTextComparator;
 import org.junit.Test;
 
 public class SortTest {
@@ -14,6 +15,16 @@ public class SortTest {
 	public void testObjectListSpecifiedSort(){
 		ObjectList<String> list = ObjectList.with("ZED", "annex", "glee", "Baleful");
 		list.sort(String.CASE_INSENSITIVE_ORDER);
+		System.out.println(list);
+	}
+	@Test
+	public void testNaturalTextSort(){
+		// Tommy is me! Satchmo is my cat!
+		ObjectList<String> list = ObjectList.with("tommy1", "tommy2", "tommy11", "tommy22", "satchmo1", "satchmo9000", "satchmo10000");
+		list.sort(NaturalTextComparator.CASE_SENSITIVE);
+		System.out.println(list);
+		list.addAll(new String[]{"Tommy0", "Tommy3", "Tommy10", "Tommy21", "Satchmo0", "Satchmo9001", "Satchmo10001"});
+		list.sort(NaturalTextComparator.CASE_INSENSITIVE);
 		System.out.println(list);
 	}
 }
