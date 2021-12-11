b011971826e6a0727febc5c0e3217e4d541b92cf
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-12-10 20:38:26 -0800

    All the Object-to-primitive maps iterate correctly.

--- src/test/java/com/github/tommyettinger/ds/test/IteratorTest.java
+++ src/test/java/com/github/tommyettinger/ds/test/IteratorTest.java
@@ -1,7 +1,13 @@
 package com.github.tommyettinger.ds.test;
 
 import com.github.tommyettinger.ds.ObjectDeque;
+import com.github.tommyettinger.ds.ObjectFloatMap;
+import com.github.tommyettinger.ds.ObjectFloatOrderedMap;
+import com.github.tommyettinger.ds.ObjectIntMap;
+import com.github.tommyettinger.ds.ObjectIntOrderedMap;
 import com.github.tommyettinger.ds.ObjectList;
+import com.github.tommyettinger.ds.ObjectLongMap;
+import com.github.tommyettinger.ds.ObjectLongOrderedMap;
 import com.github.tommyettinger.ds.ObjectObjectMap;
 import com.github.tommyettinger.ds.ObjectObjectOrderedMap;
 import com.github.tommyettinger.ds.ObjectOrderedSet;
@@ -16,6 +22,15 @@ public class IteratorTest {
 	public static final String[] strings = {
 		"alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta", "iota", "kappa", "lambda",
 		"mu", "nu", "xi", "omicron", "pi", "rho", "sigma", "tau", "upsilon", "phi", "chi", "psi", "omega"
+	};
+	public static final int[] ints = {
+		'Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ζ', 'Η', 'Θ', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ρ', 'Σ', 'Τ', 'Υ', 'Φ', 'Χ', 'Ψ', 'Ω'
+	};
+	public static final long[] longs = {
+		'Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ζ', 'Η', 'Θ', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ρ', 'Σ', 'Τ', 'Υ', 'Φ', 'Χ', 'Ψ', 'Ω'
+	};
+	public static final float[] floats = {
+		'Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ζ', 'Η', 'Θ', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ρ', 'Σ', 'Τ', 'Υ', 'Φ', 'Χ', 'Ψ', 'Ω'
 	};
 
 	@Test
@@ -126,4 +141,111 @@ public class IteratorTest {
 		Assert.assertEquals(size, counter);
 	}
 
+	@Test
+	public void testObjectIntMapIterator() {
+		ObjectIntMap<String> data = new ObjectIntMap<>(strings, ints);
+		int counter = 0, size = data.size();
+		for(ObjectIntMap.Entry<String> item : data){
+			Assert.assertNotNull(item);
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+		counter = 0;
+		Iterator<ObjectIntMap.Entry<String>> it = data.iterator();
+		for (; it.hasNext();) {
+			ObjectIntMap.Entry<String> item = it.next();
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+	}
+
+	@Test
+	public void testObjectIntOrderedMapIterator() {
+		ObjectIntOrderedMap<String> data = new ObjectIntOrderedMap<>(strings, ints);
+		int counter = 0, size = data.size();
+		for(ObjectIntMap.Entry<String> item : data){
+			Assert.assertNotNull(item);
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+		counter = 0;
+		Iterator<ObjectIntMap.Entry<String>> it = data.iterator();
+		for (; it.hasNext();) {
+			ObjectIntMap.Entry<String> item = it.next();
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+	}
+
+	@Test
+	public void testObjectLongMapIterator() {
+		ObjectLongMap<String> data = new ObjectLongMap<>(strings, longs);
+		int counter = 0, size = data.size();
+		for(ObjectLongMap.Entry<String> item : data){
+			Assert.assertNotNull(item);
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+		counter = 0;
+		Iterator<ObjectLongMap.Entry<String>> it = data.iterator();
+		for (; it.hasNext();) {
+			ObjectLongMap.Entry<String> item = it.next();
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+	}
+
+	@Test
+	public void testObjectLongOrderedMapIterator() {
+		ObjectLongOrderedMap<String> data = new ObjectLongOrderedMap<>(strings, longs);
+		int counter = 0, size = data.size();
+		for(ObjectLongMap.Entry<String> item : data){
+			Assert.assertNotNull(item);
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+		counter = 0;
+		Iterator<ObjectLongMap.Entry<String>> it = data.iterator();
+		for (; it.hasNext();) {
+			ObjectLongMap.Entry<String> item = it.next();
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+	}
+
+	@Test
+	public void testObjectFloatMapIterator() {
+		ObjectFloatMap<String> data = new ObjectFloatMap<>(strings, floats);
+		int counter = 0, size = data.size();
+		for(ObjectFloatMap.Entry<String> item : data){
+			Assert.assertNotNull(item);
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+		counter = 0;
+		Iterator<ObjectFloatMap.Entry<String>> it = data.iterator();
+		for (; it.hasNext();) {
+			ObjectFloatMap.Entry<String> item = it.next();
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+	}
+
+	@Test
+	public void testObjectFloatOrderedMapIterator() {
+		ObjectFloatOrderedMap<String> data = new ObjectFloatOrderedMap<>(strings, floats);
+		int counter = 0, size = data.size();
+		for(ObjectFloatMap.Entry<String> item : data){
+			Assert.assertNotNull(item);
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+		counter = 0;
+		Iterator<ObjectFloatMap.Entry<String>> it = data.iterator();
+		for (; it.hasNext();) {
+			ObjectFloatMap.Entry<String> item = it.next();
+			++counter;
+		}
+		Assert.assertEquals(size, counter);
+	}
 }
