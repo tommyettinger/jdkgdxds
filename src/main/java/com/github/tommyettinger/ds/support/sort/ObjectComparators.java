b36467586e9b581da935e26df860dd0814c07413
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-01-01 00:47:15 -0800

    Happy New Year! Update copyright to 2022.
    This also adds a CuckooObjectMap derivative, Cuckoo2ObjectMap, that acts like the one in Kryo (same API), but has some fixes for the stash resizing issue.

--- src/main/java/com/github/tommyettinger/ds/support/sort/ObjectComparators.java
+++ src/main/java/com/github/tommyettinger/ds/support/sort/ObjectComparators.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2002-2021 Sebastiano Vigna
+ * Copyright (c) 2022 See AUTHORS file.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,19 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *
- *
- * For the sorting and binary search code:
- *
- * Copyright (C) 1999 CERN - European Organization for Nuclear Research.
- *
- *   Permission to use, copy, modify, distribute and sell this software and
- *   its documentation for any purpose is hereby granted without fee,
- *   provided that the above copyright notice appear in all copies and that
- *   both that copyright notice and this permission notice appear in
- *   supporting documentation. CERN makes no representations about the
- *   suitability of this software for any purpose. It is provided "as is"
- *   without expressed or implied warranty.
  */
 package com.github.tommyettinger.ds.support.sort;
 
