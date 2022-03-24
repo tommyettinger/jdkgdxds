00c0a41303b8f5cde69f84ee167e91c4360bf263
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-03-24 15:15:33 -0700

    Verify different removeAll() behavior...
    ObjectList acts like ArrayList, but differently from IntList, and so probably differently from all primitive lists.

---
+++ src/test/java/com/github/tommyettinger/ds/test/ListRemoveTest.java
@@ -0,0 +1,65 @@
+package com.github.tommyettinger.ds.test;
+
+import com.github.tommyettinger.ds.IntList;
+import com.github.tommyettinger.ds.ObjectList;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class ListRemoveTest {
+	@Test(expected = AssertionError.class)
+	public void testArrayListRemoveAll (){
+		ArrayList<String> list = new ArrayList<>(ObjectList.with(
+			"foo", "bar", "baz", "quux",
+			"foo", "bar", "baz", "quux",
+			"foo", "bar", "baz", "quux"));
+		ArrayList<String> remover = new ArrayList<>(ObjectList.with("foo", "bar", "baz", "quux"));
+		Assert.assertEquals(list.size(), 12);
+		list.removeAll(remover);
+		Assert.assertEquals(list.size(), 8);
+		System.out.println(list);
+		list.removeAll(ObjectList.with("foo", "foo"));
+		Assert.assertEquals(list.size(), 6);
+		Assert.assertFalse(list.contains("foo"));
+		Assert.assertTrue(list.contains("bar"));
+		Assert.assertTrue(list.contains("baz"));
+		Assert.assertTrue(list.contains("quux"));
+	}
+	@Test(expected = AssertionError.class)
+	public void testObjectListRemoveAll (){
+		ObjectList<String> list = ObjectList.with(
+			"foo", "bar", "baz", "quux",
+			"foo", "bar", "baz", "quux",
+			"foo", "bar", "baz", "quux");
+		ObjectList<String> remover = ObjectList.with("foo", "bar", "baz", "quux");
+		Assert.assertEquals(list.size(), 12);
+		list.removeAll(remover);
+		Assert.assertEquals(list.size(), 8);
+		System.out.println(list);
+		list.removeAll(ObjectList.with("foo", "foo"));
+		Assert.assertEquals(list.size(), 6);
+		Assert.assertFalse(list.contains("foo"));
+		Assert.assertTrue(list.contains("bar"));
+		Assert.assertTrue(list.contains("baz"));
+		Assert.assertTrue(list.contains("quux"));
+	}
+	@Test()
+	public void testIntListRemoveAll (){
+		IntList list = IntList.with(
+			1, 2, 3, 4,
+			1, 2, 3, 4,
+			1, 2, 3, 4);
+		IntList remover = IntList.with(1, 2, 3, 4);
+		Assert.assertEquals(list.size(), 12);
+		list.removeAll(remover);
+		Assert.assertEquals(list.size(), 8);
+		System.out.println(list);
+		list.removeAll(IntList.with(1, 1));
+		Assert.assertEquals(list.size(), 6);
+		Assert.assertFalse(list.contains(1));
+		Assert.assertTrue(list.contains(2));
+		Assert.assertTrue(list.contains(3));
+		Assert.assertTrue(list.contains(4));
+	}
+}
