47eeae3652c4dff1291bda1f166d0ffbc6af32f8
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-06-01 21:53:18 -0700

    Test on badly-hashing Strings.
    Both of the current approaches take similar time with these horrible CharSequences.

--- src/test/java/com/github/tommyettinger/ds/test/PileupTest.java
+++ src/test/java/com/github/tommyettinger/ds/test/PileupTest.java
@@ -1,11 +1,14 @@
 package com.github.tommyettinger.ds.test;
 
 import com.github.tommyettinger.digital.Base;
+import com.github.tommyettinger.ds.IdentityObjectMap;
 import com.github.tommyettinger.ds.ObjectSet;
 import org.junit.Test;
 
+import javax.annotation.Nonnull;
+
 public class PileupTest {
-    public static final int LEN = 2000000;
+    public static final int LEN = 200000;
     public static String[] generateUniqueWords(int size) {
         final int numLetters = 3;
         ObjectSet<String> set = new ObjectSet<>(size, 0.8f);
@@ -142,6 +145,201 @@ public class PileupTest {
 //            }
 //        }
         String[] words = generateUniqueWords(LEN);
+        for (int i = 0; i < LEN; i++) {
+            set.add(words[i]);
+        }
+        System.out.println(System.nanoTime() - start);
+    }
+
+    private static class BadString implements CharSequence {
+        public CharSequence text;
+        public BadString () {
+            text = "aaa0";
+        }
+
+        public BadString(CharSequence text){
+            this.text = text;
+        }
+
+        @Override
+        public int hashCode () {
+            return text.charAt(0);
+        }
+
+        @Override
+        public boolean equals (Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            BadString badString = (BadString)o;
+
+            return text.equals(badString.text);
+        }
+
+        @Override
+        public int length () {
+            return text.length();
+        }
+
+        @Override
+        public char charAt (int index) {
+            return text.charAt(index);
+        }
+
+        @Nonnull
+        @Override
+        public CharSequence subSequence (int start, int end) {
+            return new BadString(text.subSequence(start, end));
+        }
+
+        @Nonnull
+        @Override
+        public String toString () {
+            return text.toString();
+        }
+    }
+    public static BadString[] generateUniqueBad(int size) {
+        final int numLetters = 3;
+        IdentityObjectMap<BadString, Object> set = new IdentityObjectMap<>(size, 0.8f);
+        char[] maker = new char[numLetters];
+        for (int i = 0; set.size() < size; ) {
+            for (int x = 0; x < 26 && set.size() < size; x++) {
+                maker[0] = (char) ('a' + x);
+                for (int y = 0; y < 26 && set.size() < size; y++) {
+                    maker[1] = (char) ('a' + y);
+                    for (int z = 0; z < 26 && set.size() < size; z++) {
+                        maker[2] = (char) ('a' + z);
+                        set.put(new BadString(String.valueOf(maker) + (i++)), null);
+                    }
+                }
+            }
+        }
+        return set.keySet().toArray(new BadString[0]);
+    }
+
+    //takes 12794336100
+    @Test
+    public void testBadStringSetOld() {
+        long start = System.nanoTime();
+        ObjectSet set = new ObjectSet(51, 0.7f) {
+            long collisionTotal = 0;
+            int longestPileup = 0;
+
+            @Override
+            protected void addResize (Object key) {
+                Object[] keyTable = this.keyTable;
+                for (int i = place(key), p = 0; ; i = i + 1 & mask) {
+                    if (keyTable[i] == null) {
+                        keyTable[i] = key;
+                        return;
+                    } else {
+                        collisionTotal++;
+                        longestPileup = Math.max(longestPileup, ++p);
+                    }
+                }
+            }
+
+            @Override
+            protected void resize (int newSize) {
+                int oldCapacity = keyTable.length;
+                threshold = (int)(newSize * loadFactor);
+                mask = newSize - 1;
+                shift = Long.numberOfLeadingZeros(mask);
+
+                Object[] oldKeyTable = keyTable;
+
+                keyTable = new Object[newSize];
+
+                collisionTotal = 0;
+                longestPileup = 0;
+
+                if (size > 0) {
+                    for (int i = 0; i < oldCapacity; i++) {
+                        Object key = oldKeyTable[i];
+                        if (key != null) {addResize(key);}
+                    }
+                }
+                System.out.println("hash multiplier: " + Base.BASE16.unsigned(hashMultiplier) + " with new size " + newSize);
+                System.out.println("total collisions: " + collisionTotal);
+                System.out.println("longest pileup: " + longestPileup);
+            }
+        };
+//        final int limit = (int)(Math.sqrt(LEN));
+//        for (int x = -limit; x < limit; x+=2) {
+//            for (int y = -limit; y < limit; y+=2) {
+//                set.add(new Vector2(x, y));
+//            }
+//        }
+        BadString[] words = generateUniqueBad(LEN);
+        for (int i = 0; i < LEN; i++) {
+            set.add(words[i]);
+        }
+        System.out.println(System.nanoTime() - start);
+    }
+
+    // takes 11830103300 ns
+    @Test
+    public void testBadStringSetNew() {
+        long start = System.nanoTime();
+        ObjectSet set = new ObjectSet(51, 0.7f) {
+            long collisionTotal = 0;
+            int longestPileup = 0;
+
+            @Override
+            protected void addResize (Object key) {
+                Object[] keyTable = this.keyTable;
+                for (int i = place(key), p = 0; ; i = i + 1 & mask) {
+                    if (keyTable[i] == null) {
+                        keyTable[i] = key;
+                        return;
+                    } else {
+                        collisionTotal++;
+                        longestPileup = Math.max(longestPileup, ++p);
+                    }
+                }
+            }
+
+            @Override
+            protected void resize (int newSize) {
+                int oldCapacity = keyTable.length;
+                threshold = (int)(newSize * loadFactor);
+                mask = newSize - 1;
+                shift = Long.numberOfLeadingZeros(mask);
+
+                // multiplier from Steele and Vigna, Computationally Easy, Spectrally Good Multipliers for Congruential
+                // Pseudorandom Number Generators
+//                hashMultiplier *= 0xF1357AEA2E62A9C5L;
+                // ensures hashMultiplier is never too small, and is always odd
+//                hashMultiplier |= 0x0000010000000001L;
+                hashMultiplier += 0x20008L;
+
+                Object[] oldKeyTable = keyTable;
+
+                keyTable = new Object[newSize];
+
+                collisionTotal = 0;
+                longestPileup = 0;
+
+                if (size > 0) {
+                    for (int i = 0; i < oldCapacity; i++) {
+                        Object key = oldKeyTable[i];
+                        if (key != null) {addResize(key);}
+                    }
+                }
+                System.out.println("hash multiplier: " + Base.BASE16.unsigned(hashMultiplier) + " with new size " + newSize);
+                System.out.println("total collisions: " + collisionTotal);
+                System.out.println("longest pileup: " + longestPileup);
+            }
+        };
+//        final int limit = (int)(Math.sqrt(LEN));
+//        for (int x = -limit; x < limit; x+=2) {
+//            for (int y = -limit; y < limit; y+=2) {
+//                set.add(new Vector2(x, y));
+//            }
+//        }
+        BadString[] words = generateUniqueBad(LEN);
         for (int i = 0; i < LEN; i++) {
             set.add(words[i]);
         }
