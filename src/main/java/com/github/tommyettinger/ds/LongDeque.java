88f0fb1d92828d32fa1da2a3a1a9b5d2d0c6163f
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-10-24 20:07:04 -0700

    Now fixes for LongDeque.

--- src/main/java/com/github/tommyettinger/ds/LongDeque.java
+++ src/main/java/com/github/tommyettinger/ds/LongDeque.java
@@ -150,10 +150,10 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 			values = this.values;
 		}
 
-		values[tail++] = item;
 		if (tail == values.length) {
 			tail = 0;
 		}
+		values[tail++] = item;
 		size++;
 	}
 
@@ -846,12 +846,12 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 		long value;
 		if (head < tail) { // index is between head and tail.
 			value = values[index];
-			System.arraycopy(values, index + 1, values, index, tail - index);
+			System.arraycopy(values, index + 1, values, index, tail - index - 1);
 			this.tail--;
 		} else if (index >= values.length) { // index is between 0 and tail.
 			index -= values.length;
 			value = values[index];
-			System.arraycopy(values, index + 1, values, index, tail - index);
+			System.arraycopy(values, index + 1, values, index, tail - index - 1);
 			this.tail--;
 		} else { // index is between head and values.length.
 			value = values[index];
@@ -975,7 +975,7 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 	 * iteration, use {@link LongDequeIterator#LongDequeIterator(LongDeque)}.
 	 */
 	@Override
-	public PrimitiveIterator.OfLong iterator () {
+	public LongDequeIterator iterator () {
 		if (iterator1 == null || iterator2 == null) {
 			iterator1 = new LongDequeIterator(this);
 			iterator2 = new LongDequeIterator(this);
@@ -1002,7 +1002,7 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 	 *
 	 * @return an iterator over the elements in this deque in reverse sequence
 	 */
-	public PrimitiveIterator.OfLong descendingIterator () {
+	public LongDequeIterator descendingIterator () {
 		if (descendingIterator1 == null || descendingIterator2 == null) {
 			descendingIterator1 = new LongDequeIterator(this, true);
 			descendingIterator2 = new LongDequeIterator(this, true);
@@ -1030,8 +1030,10 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 		StringBuilder sb = new StringBuilder(64);
 		sb.append('[');
 		sb.append(values[head]);
-		for (int i = (head + 1) % values.length; i != tail; i = (i + 1) % values.length) {
+		for (int i = (head + 1) % values.length; i != tail;) {
 			sb.append(", ").append(values[i]);
+			if(++i == tail) break;
+			if(i == values.length) i = 0;
 		}
 		sb.append(']');
 		return sb.toString();
@@ -1046,8 +1048,11 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 
 		StringBuilder sb = new StringBuilder(64);
 		sb.append(values[head]);
-		for (int i = (head + 1) % values.length; i != tail; i = (i + 1) % values.length)
+		for (int i = (head + 1) % values.length; i != tail;) {
 			sb.append(separator).append(values[i]);
+			if(++i == tail) break;
+			if(i == values.length) i = 0;
+		}
 		return sb.toString();
 	}
 
