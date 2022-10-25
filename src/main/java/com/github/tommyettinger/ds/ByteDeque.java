046d1a97c72f139edc9ea99a4879544a151efd69
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-10-25 00:12:27 -0700

    And ByteDeque...

--- src/main/java/com/github/tommyettinger/ds/ByteDeque.java
+++ src/main/java/com/github/tommyettinger/ds/ByteDeque.java
@@ -150,10 +150,10 @@ public class ByteDeque implements PrimitiveCollection.OfByte, Arrangeable {
 			values = this.values;
 		}
 
-		values[tail++] = item;
 		if (tail == values.length) {
 			tail = 0;
 		}
+		values[tail++] = item;
 		size++;
 	}
 
@@ -845,12 +845,12 @@ public class ByteDeque implements PrimitiveCollection.OfByte, Arrangeable {
 		byte value;
 		if (head < tail) { // index is between head and tail.
 			value = values[index];
-			System.arraycopy(values, index + 1, values, index, tail - index);
+			System.arraycopy(values, index + 1, values, index, tail - index - 1);
 			this.tail--;
 		} else if (index >= values.length) { // index is between 0 and tail.
 			index -= values.length;
 			value = values[index];
-			System.arraycopy(values, index + 1, values, index, tail - index);
+			System.arraycopy(values, index + 1, values, index, tail - index - 1);
 			this.tail--;
 		} else { // index is between head and values.length.
 			value = values[index];
@@ -974,7 +974,7 @@ public class ByteDeque implements PrimitiveCollection.OfByte, Arrangeable {
 	 * iteration, use {@link ByteDequeIterator#ByteDequeIterator(ByteDeque)}.
 	 */
 	@Override
-	public ByteIterator iterator () {
+	public ByteDequeIterator iterator () {
 		if (iterator1 == null || iterator2 == null) {
 			iterator1 = new ByteDequeIterator(this);
 			iterator2 = new ByteDequeIterator(this);
@@ -1001,7 +1001,7 @@ public class ByteDeque implements PrimitiveCollection.OfByte, Arrangeable {
 	 *
 	 * @return an iterator over the elements in this deque in reverse sequence
 	 */
-	public ByteIterator descendingIterator () {
+	public ByteDequeIterator descendingIterator () {
 		if (descendingIterator1 == null || descendingIterator2 == null) {
 			descendingIterator1 = new ByteDequeIterator(this, true);
 			descendingIterator2 = new ByteDequeIterator(this, true);
@@ -1029,8 +1029,10 @@ public class ByteDeque implements PrimitiveCollection.OfByte, Arrangeable {
 		StringBuilder sb = new StringBuilder(64);
 		sb.append('[');
 		sb.append(values[head]);
-		for (int i = (head + 1) % values.length; i != tail; i = (i + 1) % values.length) {
+		for (int i = (head + 1) % values.length; i != tail;) {
 			sb.append(", ").append(values[i]);
+			if(++i == tail) break;
+			if(i == values.length) i = 0;
 		}
 		sb.append(']');
 		return sb.toString();
@@ -1045,8 +1047,11 @@ public class ByteDeque implements PrimitiveCollection.OfByte, Arrangeable {
 
 		StringBuilder sb = new StringBuilder(64);
 		sb.append(values[head]);
-		for (int i = (head + 1) % values.length; i != tail; i = (i + 1) % values.length)
+		for (int i = (head + 1) % values.length; i != tail;) {
 			sb.append(separator).append(values[i]);
+			if(++i == tail) break;
+			if(i == values.length) i = 0;
+		}
 		return sb.toString();
 	}
 
