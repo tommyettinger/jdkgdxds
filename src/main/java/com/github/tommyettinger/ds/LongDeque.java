a261049491fb1de8166f0f81bff3447563b5b367
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-10-26 17:55:00 -0700

    Polling stuff, LongDeque.

--- src/main/java/com/github/tommyettinger/ds/LongDeque.java
+++ src/main/java/com/github/tommyettinger/ds/LongDeque.java
@@ -274,10 +274,6 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 	 * @param t the element to add
 	 * @return {@code true} if the element was added to this deque, else
 	 * {@code false}
-	 * @throws ClassCastException       if the class of the specified element
-	 *                                  prevents it from being added to this deque
-	 * @throws NullPointerException     if the specified element is null and this
-	 *                                  deque does not permit null elements
 	 * @throws IllegalArgumentException if some property of the specified
 	 *                                  element prevents it from being added to this deque
 	 */
@@ -296,10 +292,6 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 	 * @param t the element to add
 	 * @return {@code true} if the element was added to this deque, else
 	 * {@code false}
-	 * @throws ClassCastException       if the class of the specified element
-	 *                                  prevents it from being added to this deque
-	 * @throws NullPointerException     if the specified element is null and this
-	 *                                  deque does not permit null elements
 	 * @throws IllegalArgumentException if some property of the specified
 	 *                                  element prevents it from being added to this deque
 	 */
@@ -311,22 +303,47 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 
 	/**
 	 * Retrieves and removes the first element of this deque,
-	 * or returns {@code null} if this deque is empty.
+	 * or returns {@link #getDefaultValue() defaultValue} if this deque is empty.
 	 *
-	 * @return the head of this deque, or {@code null} if this deque is empty
+	 * @return the head of this deque, or {@link #getDefaultValue() defaultValue} if this deque is empty
 	 */
 	public long pollFirst () {
-		return removeFirst();
+		if(size == 0)
+			return defaultValue;
+		final long[] values = this.values;
+
+		final long result = values[head];
+		head++;
+		if (head == values.length) {
+			head = 0;
+		}
+		size--;
+
+		return result;
 	}
 
 	/**
 	 * Retrieves and removes the last element of this deque,
-	 * or returns {@code null} if this deque is empty.
+	 * or returns {@link #getDefaultValue() defaultValue} if this deque is empty.
 	 *
-	 * @return the tail of this deque, or {@code null} if this deque is empty
+	 * @return the tail of this deque, or {@link #getDefaultValue() defaultValue} if this deque is empty
 	 */
 	public long pollLast () {
-		return removeLast();
+		if (size == 0) {
+			return defaultValue;
+		}
+
+		final long[] values = this.values;
+		int tail = this.tail;
+		tail--;
+		if (tail == -1) {
+			tail = values.length - 1;
+		}
+		final long result = values[tail];
+		this.tail = tail;
+		size--;
+
+		return result;
 	}
 
 	/**
@@ -392,18 +409,12 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 	 * Removes the first occurrence of the specified element from this deque.
 	 * If the deque does not contain the element, it is unchanged.
 	 * More formally, removes the first element {@code e} such that
-	 * {@code Objects.equals(o, e)} (if such an element exists).
+	 * {@code o == e)} (if such an element exists).
 	 * Returns {@code true} if this deque contained the specified element
 	 * (or equivalently, if this deque changed as a result of the call).
 	 *
 	 * @param o element to be removed from this deque, if present
 	 * @return {@code true} if an element was removed as a result of this call
-	 * @throws ClassCastException   if the class of the specified element
-	 *                              is incompatible with this deque
-	 *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-	 * @throws NullPointerException if the specified element is null and this
-	 *                              deque does not permit null elements
-	 *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
 	 */
 	public boolean removeFirstOccurrence (long o) {
 		return removeValue(o);
@@ -413,18 +424,12 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 	 * Removes the last occurrence of the specified element from this deque.
 	 * If the deque does not contain the element, it is unchanged.
 	 * More formally, removes the last element {@code e} such that
-	 * {@code Objects.equals(o, e)} (if such an element exists).
+	 * {@code o == e} (if such an element exists).
 	 * Returns {@code true} if this deque contained the specified element
 	 * (or equivalently, if this deque changed as a result of the call).
 	 *
 	 * @param o element to be removed from this deque, if present
 	 * @return {@code true} if an element was removed as a result of this call
-	 * @throws ClassCastException   if the class of the specified element
-	 *                              is incompatible with this deque
-	 *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-	 * @throws NullPointerException if the specified element is null and this
-	 *                              deque does not permit null elements
-	 *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
 	 */
 	public boolean removeLastOccurrence (long o) {
 		return removeLastValue(o);
@@ -445,10 +450,6 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 	 * @return {@code true} (as specified by {@link Collection#add})
 	 * @throws IllegalStateException    if the element cannot be added at this
 	 *                                  time due to capacity restrictions
-	 * @throws ClassCastException       if the class of the specified element
-	 *                                  prevents it from being added to this deque
-	 * @throws NullPointerException     if the specified element is null and this
-	 *                                  deque does not permit null elements
 	 * @throws IllegalArgumentException if some property of the specified
 	 *                                  element prevents it from being added to this deque
 	 */
@@ -537,10 +538,6 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 	 * @param t the element to add
 	 * @return {@code true} if the element was added to this deque, else
 	 * {@code false}
-	 * @throws ClassCastException       if the class of the specified element
-	 *                                  prevents it from being added to this deque
-	 * @throws NullPointerException     if the specified element is null and this
-	 *                                  deque does not permit null elements
 	 * @throws IllegalArgumentException if some property of the specified
 	 *                                  element prevents it from being added to this deque
 	 */
@@ -576,7 +573,7 @@ public class LongDeque implements PrimitiveCollection.OfLong, Arrangeable {
 	 * this deque is empty
 	 */
 	public long poll () {
-		return removeFirst();
+		return pollFirst();
 	}
 
 	/**
