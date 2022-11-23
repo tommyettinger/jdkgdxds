75bbdc0c6fe9da28aded280f3bea897a7217bc58
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-11-22 19:53:13 -0800

    And of course, ObjectFloatMap.

--- src/main/java/com/github/tommyettinger/ds/ObjectFloatMap.java
+++ src/main/java/com/github/tommyettinger/ds/ObjectFloatMap.java
@@ -867,6 +867,19 @@ public class ObjectFloatMap<K> implements Iterable<ObjectFloatMap.Entry<K>> {
 		@Nullable public K key;
 		public float value;
 
+		public Entry () {
+		}
+
+		public Entry (@Nullable K key, float value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		public Entry (Entry<K> entry) {
+			this.key = entry.key;
+			this.value = entry.value;
+		}
+
 		@Override
 		public String toString () {
 			return key + "=" + value;
@@ -1104,6 +1117,77 @@ public class ObjectFloatMap<K> implements Iterable<ObjectFloatMap.Entry<K>> {
 		public int size () {
 			return iter.map.size;
 		}
+
+		@Override
+		public int hashCode () {
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			iter.reset();
+			int hc = super.hashCode();
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return hc;
+		}
+
+		/**
+		 * The iterator is reused by this data structure, and you can reset it
+		 * back to the start of the iteration order using this.
+		 */
+		public void resetIterator () {
+			iter.reset();
+		}
+
+		/**
+		 * Returns a new {@link ObjectList} containing the remaining items.
+		 * Does not change the position of this iterator.
+		 */
+		public ObjectList<Entry<K>> toList () {
+			ObjectList<Entry<K>> list = new ObjectList<>(iter.map.size);
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			while (iter.hasNext) {list.add(new Entry<>(iter.next()));}
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return list;
+		}
+
+		/**
+		 * Append the remaining items that this can iterate through into the given Collection.
+		 * Does not change the position of this iterator.
+		 * @param coll any modifiable Collection; may have items appended into it
+		 * @return the given collection
+		 */
+		public Collection<Entry<K>> appendInto(Collection<Entry<K>> coll) {
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			while (iter.hasNext) {coll.add(new Entry<>(iter.next()));}
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return coll;
+		}
+
+		/**
+		 * Append the remaining items that this can iterate through into the given Map.
+		 * Does not change the position of this iterator. Note that a Map is not a Collection.
+		 * @param coll any modifiable Map; may have items appended into it
+		 * @return the given map
+		 */
+		public ObjectFloatMap<K> appendInto(ObjectFloatMap<K> coll) {
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			while (iter.hasNext) {
+				iter.next();
+				assert iter.entry.key != null;
+				coll.put(iter.entry.key, iter.entry.value);
+			}
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return coll;
+		}
 	}
 
 	public static class Values<K> implements PrimitiveCollection.OfFloat {
@@ -1143,6 +1227,56 @@ public class ObjectFloatMap<K> implements Iterable<ObjectFloatMap.Entry<K>> {
 			iter = new ValueIterator<>(map);
 		}
 
+		@Override
+		public int hashCode () {
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			iter.reset();
+			int hc = super.hashCode();
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return hc;
+		}
+
+		/**
+		 * The iterator is reused by this data structure, and you can reset it
+		 * back to the start of the iteration order using this.
+		 */
+		public void resetIterator () {
+			iter.reset();
+		}
+
+		/**
+		 * Returns a new {@link ObjectList} containing the remaining items.
+		 * Does not change the position of this iterator.
+		 */
+		public FloatList toList () {
+			FloatList list = new FloatList(iter.map.size);
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			while (iter.hasNext) {list.add(iter.nextFloat());}
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return list;
+		}
+
+		/**
+		 * Append the remaining items that this can iterate through into the given Collection.
+		 * Does not change the position of this iterator.
+		 * @param coll any modifiable Collection; may have items appended into it
+		 * @return the given collection
+		 */
+		public PrimitiveCollection.OfFloat appendInto(PrimitiveCollection.OfFloat coll) {
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			while (iter.hasNext) {coll.add(iter.nextFloat());}
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return coll;
+		}
 	}
 
 	public static class Keys<K> extends AbstractSet<K> {
@@ -1165,6 +1299,57 @@ public class ObjectFloatMap<K> implements Iterable<ObjectFloatMap.Entry<K>> {
 		@Override
 		public int size () {
 			return iter.map.size;
+		}
+
+		@Override
+		public int hashCode () {
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			iter.reset();
+			int hc = super.hashCode();
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return hc;
+		}
+
+		/**
+		 * The iterator is reused by this data structure, and you can reset it
+		 * back to the start of the iteration order using this.
+		 */
+		public void resetIterator () {
+			iter.reset();
+		}
+
+		/**
+		 * Returns a new {@link ObjectList} containing the remaining items.
+		 * Does not change the position of this iterator.
+		 */
+		public ObjectList<K> toList () {
+			ObjectList<K> list = new ObjectList<>(iter.map.size);
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			while (iter.hasNext) {list.add(iter.next());}
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return list;
+		}
+
+		/**
+		 * Append the remaining items that this can iterate through into the given Collection.
+		 * Does not change the position of this iterator.
+		 * @param coll any modifiable Collection; may have items appended into it
+		 * @return the given collection
+		 */
+		public Collection<K> appendInto(Collection<K> coll) {
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			while (iter.hasNext) {coll.add(iter.next());}
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return coll;
 		}
 	}
 
