cfe7fa907321f87cc9f49e85d432c0333664b04a
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-11-25 20:18:40 -0800

    Generate spiral of points, test on it.
    This one often has negative components, so it could be used for something like the hashCode() on a GridPoint2.

--- src/test/java/com/github/tommyettinger/ds/test/PileupTest.java
+++ src/test/java/com/github/tommyettinger/ds/test/PileupTest.java
@@ -1165,6 +1165,37 @@ public class PileupTest {
         }
     }
 
+    /**
+     * Generates an array of Point2 points in a spiral order, starting at (0,0) and moving in a square
+     * spiral through all quadrants equally often. This can be a good worst-case test for hash tables
+     * that don't adequately handle negative inputs (like some point hashes that are only defined for
+     * non-negative inputs). This is different from {@link #generateVectorSpiral(int)} because it
+     * generates a 4x larger spiral, shuffles it, and expects only {@code size} elements to actually
+     * be read from it (effectively drawing a random quarter of the points to be used).
+     * Thanks to Jonathan M, <a href="https://stackoverflow.com/a/20591835">on Stack Overflow</a>.
+     *
+     * @param size
+     * @return
+     */
+    public static Point2[] generatePointSpiral (int size) {
+        WhiskerRandom random = new WhiskerRandom(size);
+        size <<= 2;
+        Point2[] result = new Point2[size];
+        for (int root = 0, index = 0; ; ++root) {
+            for (int limit = index + root + root; index <= limit; ) {
+                final int sign = -(root & 1);
+                final int big = (root * (root + 1)) - index << 1;
+                final int y = ((root + 1 >> 1) + sign ^ sign) + ((sign ^ sign + Math.min(big, 0)) >> 1);
+                final int x = ((root + 1 >> 1) + sign ^ sign) - ((sign ^ sign + Math.max(big, 0)) >> 1);
+                result[index] = new Point2(x, y);
+                if (++index >= size) {
+                    random.shuffle(result);
+                    return result;
+                }
+            }
+        }
+    }
+
     public static final long CONSTANT =
 //        0x9E3779B97F4A7C15L; //int: total collisions: 33748, longest pileup: 12, long: total collisions: 34124, longest pileup: 13
 //        0xD1B54A32D192ED03L;//long: total collisions: 33579, longest pileup: 12
@@ -1904,14 +1935,13 @@ public class PileupTest {
         }
     }
 
-
     @Test
     public void testPointSetAngles () {
         final Point2[] shells = generatePointAngles(LEN);
         IntBinaryOperator[] hashes = {
             ((x, y) -> x * 0x125493 + y * 0x19E373), // 1MS strong, 2A strong, 3MA strong
             ((x, y) -> x * 0xDEED5 + y * 0xBEA57), // 1MS strong, 2A strong, 3MA strong
-            ((x, y) -> (int)((x * 107 + y) * 0xD1B54A32D192ED03L >>> 32)), // 1MS , 2A strong, 3MA strong
+            ((x, y) -> (int)((x * 107 + y) * 0xD1B54A32D192ED03L >>> 32)), // 1MS strong, 2A strong, 3MA strong
             ((x, y) -> (x >= y ? x * (x + 8) - y + 12 : y * (y + 6) + x + 12)), // 1MS strong, 2A strong, 3MA strong
             ((x, y) -> {int n = (x >= y ? x * (x + 8) - y + 12 : y * (y + 6) + x + 12); return n ^ n >>> 1;}), // 1MS strong, 2A strong, 3MA strong
             ((x, y) -> {int n = (x >= y ? x * (x + 8) - y + 12 : y * (y + 6) + x + 12); return ((n ^ n >>> 1) * 0x9E373 ^ 0xD1B54A35) * 0x125493 ^ 0x91E10DA5;}), // 1MS strong, 2A strong, 3MA strong
@@ -1925,7 +1955,7 @@ public class PileupTest {
                 x =    ((x & 0x00f000f0) << 4) | ((x >>> 4) & 0x00f000f0) | (x & 0xf00ff00f);
                 x =    ((x & 0x0c0c0c0c) << 2) | ((x >>> 2) & 0x0c0c0c0c) | (x & 0xc3c3c3c3);
                 return ((x & 0x22222222) << 1) | ((x >>> 1) & 0x22222222) | (x & 0x99999999);
-            }), // 1MS strong, 2A fail, 3MA fail 314572/500000
+            }), // 1MS strong, 2A fail 78643/500000, 3MA fail 314572/500000
             ((x, y) -> (x ^ (y << 16 | y >>> 16))), // 1MS strong, 2A strong, 3MA strong
             ((x, y) -> (x + (y << 16 | y >>> 16))), // 1MS strong, 2A strong, 3MA strong
         };
@@ -1948,8 +1978,8 @@ public class PileupTest {
                 protected int place (Object item) {
                     Point2 p = (Point2)item;
 //                    return (int)(hash.applyAsInt(p.x, p.y) * hashMultiplier >>> shift); // option 1MS
-//                    return hash.applyAsInt(p.x, p.y) & mask; // option 2A
-                    return hash.applyAsInt(p.x, p.y) * hashMul & mask; // option 3MA
+                    return hash.applyAsInt(p.x, p.y) & mask; // option 2A
+//                    return hash.applyAsInt(p.x, p.y) * hashMul & mask; // option 3MA
                 }
 
                 @Override
@@ -2013,12 +2043,6 @@ public class PileupTest {
                     super.clear();
                 }
             };
-//        final int limit = (int)(Math.sqrt(LEN));
-//        for (int x = -limit; x < limit; x+=2) {
-//            for (int y = -limit; y < limit; y+=2) {
-//                set.add(new Vector2(x, y));
-//            }
-//        }
             try {
                 for (int i = 0; i < LEN; i++) {
                     set.add(shells[i]);
@@ -2030,6 +2054,123 @@ public class PileupTest {
             set.clear();
         }
     }
+    @Test
+    public void testPointSetSpiral () {
+        final Point2[] shells = generatePointSpiral(LEN);
+        IntBinaryOperator[] hashes = {
+            ((x, y) -> x * 0x125493 + y * 0x19E373), // 1MS strong, 2A strong, 3MA strong
+            ((x, y) -> x * 0xDEED5 + y * 0xBEA57), // 1MS very strong, 2A strong, 3MA strong
+            ((x, y) -> (int)((x * 107 + y) * 0xD1B54A32D192ED03L >>> 32)), // 1MS fail 314572/500000, 2A fail 314572/500000, 3MA fail 314572/500000
+            ((x, y) -> (x >= y ? x * (x + 8) - y + 12 : y * (y + 6) + x + 12)), // 1MS fail 314572/500000, 2A fail 39321/500000, 3MA fail 314572/500000
+            ((x, y) -> {int n = (x >= y ? x * (x + 8) - y + 12 : y * (y + 6) + x + 12); return n ^ n >>> 1;}), // 1MS fail 314572/500000, 2A fail 39321/500000, 3MA fail 314572/500000
+            ((x, y) -> {int n = (x >= y ? x * (x + 8) - y + 12 : y * (y + 6) + x + 12); return ((n ^ n >>> 1) * 0x9E373 ^ 0xD1B54A35) * 0x125493 ^ 0x91E10DA5;}), // 1MS fail 314572/500000, 2A fail 314572/500000, 3MA fail 314572/500000
+            ((x, y) -> y + ((x + y + 6) * (x + y + 7) >>> 1)), // 1MS fail 314572/500000, 2A fail 39321/500000, 3MA fail 314572/500000
+            ((x, y) -> {int n = (y + ((x + y + 6) * (x + y + 7) >> 1)); return n ^ n >>> 1;}), // 1MS fail 314572/500000, 2A fail 19660/500000, 3MA fail 314572/500000
+            ((x, y) -> {int n = (y + ((x + y + 6) * (x + y + 7) >> 1)); return ((n ^ n >>> 1) * 0x9E373 ^ 0xD1B54A35) * 0x125493 ^ 0x91E10DA5;}), // 1MS fail 314572/500000, 2A fail 314572/500000, 3MA fail 314572/500000
+            ((x, y) -> y + ((x + y) * (x + y + 1) + 36 >>> 1)), // 1MS fail 314572/500000, 2A 39321/500000, 3MA fail fail 314572/500000
+            ((x, y) -> {
+                x |= y << 16;
+                x =    ((x & 0x0000ff00) << 8) | ((x >>> 8) & 0x0000ff00) | (x & 0xff0000ff);
+                x =    ((x & 0x00f000f0) << 4) | ((x >>> 4) & 0x00f000f0) | (x & 0xf00ff00f);
+                x =    ((x & 0x0c0c0c0c) << 2) | ((x >>> 2) & 0x0c0c0c0c) | (x & 0xc3c3c3c3);
+                return ((x & 0x22222222) << 1) | ((x >>> 1) & 0x22222222) | (x & 0x99999999);
+            }), // 1MS fail 39321/500000, 2A fail 4915/500000, 3MA fail 19660/500000
+            ((x, y) -> (x ^ (y << 16 | y >>> 16))), // 1MS strong, 2A fail 2457/500000, 3MA fail 39321/500000
+            ((x, y) -> (x + (y << 16 | y >>> 16))), // 1MS very strong, 2A fail 2457/500000, 3MA fail 39321/500000
+        };
+        int index = 0;
+        for(IntBinaryOperator op : hashes) {
+            System.out.println("Working with hash " + index++ + ":");
+            final IntBinaryOperator hash = op;
+            long start = System.nanoTime();
+            ObjectSet set = new ObjectSet(51, 0.6f) {
+                long collisionTotal = 0;
+                int longestPileup = 0, allPileups = 0, pileupChecks = 0;
+                double averagePileup = 0;
 
+                //            {
+//                hashMultiplier = 0xD1B54A32D192ED03L;
+//            }
+                int hashMul = (int)(hashMultiplier & 0x1FFFFFL);
 
+                @Override
+                protected int place (Object item) {
+                    Point2 p = (Point2)item;
+                    return (int)(hash.applyAsInt(p.x, p.y) * hashMultiplier >>> shift); // option 1MS
+//                    return hash.applyAsInt(p.x, p.y) & mask; // option 2A
+//                    return hash.applyAsInt(p.x, p.y) * hashMul & mask; // option 3MA
+                }
+
+                @Override
+                protected void addResize (@Nonnull Object key) {
+                    Object[] keyTable = this.keyTable;
+                    for (int i = place(key), p = 0; ; i = i + 1 & mask) {
+                        if (keyTable[i] == null) {
+                            keyTable[i] = key;
+                            averagePileup += p;
+
+                            return;
+                        } else {
+                            collisionTotal++;
+                            longestPileup = Math.max(longestPileup, ++p);
+                        }
+                    }
+                }
+
+                @Override
+                protected void resize (int newSize) {
+                    int oldCapacity = keyTable.length;
+                    threshold = (int)(newSize * loadFactor);
+                    mask = newSize - 1;
+                    shift = Long.numberOfLeadingZeros(mask);
+
+                    // was using this in many tests
+                    // total 1788695, longest 33, average 5.686122731838816, sum 160
+                    hashMultiplier *= size + size ^ 0xF1357AEA2E62A9C5L;
+
+                    hashMul = (int)(hashMultiplier & 0x1FFFFFL);
+
+                    Object[] oldKeyTable = keyTable;
+
+                    keyTable = new Object[newSize];
+
+                    allPileups += longestPileup;
+                    pileupChecks++;
+                    collisionTotal = 0;
+                    longestPileup = 0;
+                    averagePileup = 0.0;
+
+                    if (size > 0) {
+                        for (int i = 0; i < oldCapacity; i++) {
+                            Object key = oldKeyTable[i];
+                            if (key != null) {addResize(key);}
+                        }
+                    }
+                    if(collisionTotal > 150000) throw new IllegalStateException("UH OH");
+
+//                    System.out.println("hash multiplier: " + Base.BASE16.unsigned(hashMultiplier) + " with new size " + newSize);
+//                    System.out.println("total collisions: " + collisionTotal);
+//                    System.out.println("longest pileup: " + longestPileup);
+//                    System.out.println("average pileup: " + (averagePileup / size));
+                }
+
+                @Override
+                public void clear () {
+//                    System.out.println("hash multiplier: " + Base.BASE16.unsigned(hashMultiplier) + " with final size " + size);
+                    System.out.println("total collisions: " + collisionTotal + ", longest pileup: " + longestPileup);
+                    System.out.println("total of " + pileupChecks + " pileups: " + (allPileups + longestPileup));
+                    super.clear();
+                }
+            };
+            try {
+                for (int i = 0; i < LEN; i++) {
+                    set.add(shells[i]);
+                }
+            }catch (IllegalStateException ex) {
+                System.out.println("Way too many collisions! Only got through " + set.size() + "/" + LEN);
+            }
+            System.out.println(System.nanoTime() - start);
+            set.clear();
+        }
+    }
 }
