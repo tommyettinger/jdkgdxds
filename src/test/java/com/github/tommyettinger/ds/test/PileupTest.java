466efa30afcefd866b836f74345ddcc2a591f72d
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-12-02 19:25:36 -0800

    Using method 2A with rectangles fails more.

--- src/test/java/com/github/tommyettinger/ds/test/PileupTest.java
+++ src/test/java/com/github/tommyettinger/ds/test/PileupTest.java
@@ -2830,7 +2830,7 @@ public class PileupTest {
          width 500
          hash 0, 1MS , 2A , 3MA
          hash 1, 1MS , 2A , 3MA
-         hash 2, 1MS fail 78643/500000, 2A , 3MA
+         hash 2, 1MS fail 78643/500000, 2A fail 19660/500000, 3MA
          hash 3, 1MS , 2A , 3MA
          hash 4, 1MS , 2A , 3MA
          hash 5, 1MS , 2A , 3MA
@@ -2839,71 +2839,71 @@ public class PileupTest {
          hash 8, 1MS , 2A , 3MA
          hash 9, 1MS , 2A , 3MA
          hash 10, 1MS , 2A , 3MA
-         hash 11, 1MS , 2A , 3MA
-         hash 12, 1MS , 2A , 3MA
-         hash 13, 1MS fail 314572/500000, 2A , 3MA
+         hash 11, 1MS , 2A fail 1228/500000, 3MA
+         hash 12, 1MS , 2A fail 1228/500000, 3MA
+         hash 13, 1MS fail 314572/500000, 2A fail 39321/500000, 3MA
          hash 14, 1MS , 2A , 3MA
          hash 15, 1MS , 2A , 3MA
          */
 
         /*
          width 200
-         hash 0, 1MS , 2A , 3MA
+         hash 0, 1MS , 2A fail 314572/500000, 3MA
          hash 1, 1MS , 2A , 3MA
-         hash 2, 1MS fail 78643/500000, 2A , 3MA
-         hash 3, 1MS , 2A , 3MA
-         hash 4, 1MS , 2A , 3MA
+         hash 2, 1MS fail 78643/500000, 2A fail 9830/500000, 3MA
+         hash 3, 1MS , 2A fail 157286/500000, 3MA
+         hash 4, 1MS , 2A fail 157286/500000, 3MA
          hash 5, 1MS , 2A , 3MA
-         hash 6, 1MS , 2A , 3MA
-         hash 7, 1MS , 2A , 3MA
+         hash 6, 1MS , 2A fail 157286/500000, 3MA
+         hash 7, 1MS , 2A fail 157286/500000, 3MA
          hash 8, 1MS , 2A , 3MA
-         hash 9, 1MS , 2A , 3MA
-         hash 10, 1MS , 2A , 3MA
-         hash 11, 1MS , 2A , 3MA
-         hash 12, 1MS , 2A , 3MA
-         hash 13, 1MS , 2A , 3MA
+         hash 9, 1MS , 2A fail 157286/500000, 3MA
+         hash 10, 1MS , 2A fail 157286/500000, 3MA
+         hash 11, 1MS , 2A fail 1228/500000, 3MA
+         hash 12, 1MS , 2A fail 1228/500000, 3MA
+         hash 13, 1MS , 2A fail 78643/500000, 3MA
          hash 14, 1MS , 2A , 3MA
          hash 15, 1MS , 2A , 3MA
          */
 
         /*
          width 100
-         hash 0, 1MS , 2A , 3MA
+         hash 0, 1MS , 2A fail 314572/500000, 3MA
          hash 1, 1MS , 2A , 3MA
-         hash 2, 1MS fail 78643/500000, 2A , 3MA
-         hash 3, 1MS , 2A , 3MA
-         hash 4, 1MS , 2A , 3MA
+         hash 2, 1MS fail 78643/500000, 2A fail 9830/500000, 3MA
+         hash 3, 1MS , 2A fail 157286/500000, 3MA
+         hash 4, 1MS , 2A fail 157286/500000, 3MA
          hash 5, 1MS , 2A , 3MA
-         hash 6, 1MS , 2A , 3MA
-         hash 7, 1MS , 2A , 3MA
+         hash 6, 1MS , 2A fail 157286/500000, 3MA
+         hash 7, 1MS , 2A fail 157286/500000, 3MA
          hash 8, 1MS , 2A , 3MA
-         hash 9, 1MS , 2A , 3MA
-         hash 10, 1MS , 2A , 3MA
-         hash 11, 1MS , 2A , 3MA
-         hash 12, 1MS , 2A , 3MA
-         hash 13, 1MS , 2A , 3MA
+         hash 9, 1MS , 2A fail 157286/500000, 3MA
+         hash 10, 1MS , 2A fail 39321/500000, 3MA
+         hash 11, 1MS , 2A fail 614/500000, 3MA
+         hash 12, 1MS , 2A fail 614/500000, 3MA
+         hash 13, 1MS , 2A fail 157286/500000, 3MA
          hash 14, 1MS , 2A , 3MA
          hash 15, 1MS , 2A , 3MA
          */
 
         /*
          width 50
-         hash 0, 1MS , 2A , 3MA
+         hash 0, 1MS , 2A fail 314572/500000, 3MA
          hash 1, 1MS , 2A , 3MA
-         hash 2, 1MS fail 78643/500000, 2A , 3MA
-         hash 3, 1MS , 2A , 3MA
-         hash 4, 1MS , 2A , 3MA
+         hash 2, 1MS fail 78643/500000, 2A fail 9830/500000, 3MA
+         hash 3, 1MS , 2A fail 157286/500000, 3MA
+         hash 4, 1MS , 2A fail 157286/500000, 3MA
          hash 5, 1MS , 2A , 3MA
-         hash 6, 1MS , 2A , 3MA
-         hash 7, 1MS , 2A , 3MA
+         hash 6, 1MS , 2A fail 157286/500000, 3MA
+         hash 7, 1MS , 2A fail 157286/500000, 3MA
          hash 8, 1MS , 2A , 3MA
-         hash 9, 1MS , 2A , 3MA
-         hash 10, 1MS , 2A , 3MA 
-         hash 11, 1MS , 2A , 3MA 
-         hash 12, 1MS , 2A , 3MA 
-         hash 13, 1MS , 2A , 3MA
+         hash 9, 1MS , 2A fail 157286/500000, 3MA
+         hash 10, 1MS , 2A fail 9830/500000, 3MA
+         hash 11, 1MS , 2A fail 614/500000, 3MA
+         hash 12, 1MS , 2A fail 614/500000, 3MA
+         hash 13, 1MS , 2A fail 157286/500000, 3MA
          hash 14, 1MS , 2A , 3MA
-         hash 15, 1MS , 2A , 3MA
+         hash 15, 1MS , 2A fail 157286/500000, 3MA
          */
 
 
@@ -2925,8 +2925,8 @@ public class PileupTest {
                     @Override
                     protected int place (Object item) {
                         final Point2 p = (Point2)item;
-                        return (int)(hash.applyAsInt(p.x, p.y) * hashMultiplier >>> shift); // option 1MS
-//                        return hash.applyAsInt(p.x, p.y) & mask; // option 2A
+//                        return (int)(hash.applyAsInt(p.x, p.y) * hashMultiplier >>> shift); // option 1MS
+                        return hash.applyAsInt(p.x, p.y) & mask; // option 2A
 //                        return hash.applyAsInt(p.x, p.y) * hashMul & mask; // option 3MA
                     }
 
