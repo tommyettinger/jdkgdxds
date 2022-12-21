431824b9ec1da4c55ab32ac79de655b5bd1c90d9
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-12-20 23:24:30 -0800

    The hashes in the word list are great -- ruin 'em.
    The goal here is to find a good-bad way to have BadString have mostly-unique-sometimes hashes that still quite bad.

--- src/test/java/com/github/tommyettinger/ds/test/WordListStats.java
+++ src/test/java/com/github/tommyettinger/ds/test/WordListStats.java
@@ -17,24 +17,37 @@
 
 package com.github.tommyettinger.ds.test;
 
+import com.github.tommyettinger.digital.BitConversion;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.IntUnaryOperator;
 
 public class WordListStats {
+	public static final IntUnaryOperator[] worse = {
+		h -> h, // hash 0, identity
+		h -> h & (h ^ h >>> 1), // hash 1, mask with its own gray code
+		h -> h & (h << 21 | h >>> 11) & (h << 13 | h >>> 19), // hash 2, ARR
+		h -> h & (BitConversion.imul(h, h) | 1), // hash 3, AQO
+		h -> h & (h ^ h << 1), // hash 4, mask with a sort of gray-ish code
+	};
 	public static void main(String[] args) throws IOException {
 		final List<String> words = Files.readAllLines(Paths.get("src/test/resources/word_list.txt"));
 		int wordCount = words.size();
-		long sum = words.parallelStream().mapToLong(String::hashCode).sum();
-		double averageHashCode = sum / (double)wordCount;
-		double averageBitCount = words.parallelStream().mapToLong((s) -> Integer.bitCount(s.hashCode())).sum() / (double)wordCount;
-		double averageExtent = words.parallelStream().mapToLong((s) -> 32 - Integer.numberOfLeadingZeros(s.hashCode())).sum() / (double)wordCount;
-		System.out.printf("Number of words   : %d\n", wordCount);
-		System.out.printf("hashCode() sum    : %d\n", sum);
-		System.out.printf("hashCode() average: %10.8f\n", averageHashCode);
-		System.out.printf("bitCount() average: %10.8f\n", averageBitCount);
-		System.out.printf("extent average    : %10.8f\n", averageExtent);
+		for (int i = 0; i < worse.length; i++) {
+			IntUnaryOperator op = worse[i];
+			System.out.println("Working with hash-worsening operator " + i);
+			long sum = words.parallelStream().mapToLong(s -> op.applyAsInt(s.hashCode())).sum();
+			double averageHashCode = sum / (double)wordCount;
+			double averageBitCount = words.parallelStream().mapToLong((s) -> Integer.bitCount(op.applyAsInt(s.hashCode()))).sum() / (double)wordCount;
+			double averageExtent = words.parallelStream().mapToLong((s) -> 32 - Integer.numberOfLeadingZeros(op.applyAsInt(s.hashCode()))).sum() / (double)wordCount;
+			System.out.printf("Number of words   : %d\n", wordCount);
+			System.out.printf("hashCode() sum    : %d\n", sum);
+			System.out.printf("hashCode() average: %10.8f\n", averageHashCode);
+			System.out.printf("bitCount() average: %10.8f\n", averageBitCount);
+			System.out.printf("extent average    : %10.8f\n", averageExtent);
+		}
 	}
-
 }
