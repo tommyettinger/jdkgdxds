6547bdcad2dd98b85fcc5b26eb63eb6c5180c4c8
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-06-12 20:41:15 -0700

    EnumLongOrderedMap!

---
+++ src/test/java/com/github/tommyettinger/ds/enums/EnumLongOrderedMap.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2022-2023 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.ds.enums;
+
+import com.github.tommyettinger.ds.ObjectLongMap;
+import com.github.tommyettinger.ds.ObjectLongOrderedMap;
+import com.github.tommyettinger.ds.PrimitiveCollection;
+import com.github.tommyettinger.ds.Utilities;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.Collection;
+
+/**
+ * A custom variant on ObjectLongOrderedMap that always uses enum keys, which simplifies some operations.
+ */
+public class EnumLongOrderedMap<K extends Enum<K>> extends ObjectLongOrderedMap<K> {
+
+	/**
+	 * Creates a new map with an initial capacity of 51 and a load factor of {@link Utilities#getDefaultLoadFactor()}.
+	 */
+	public EnumLongOrderedMap () {
+		super();
+	}
+
+	/**
+	 * Creates a new map with the specified initial capacity and a load factor of {@link Utilities#getDefaultLoadFactor()}.
+	 * This map will hold initialCapacity items before growing the backing table.
+	 *
+	 * @param initialCapacity If not a power of two, it is increased to the next nearest power of two.
+	 */
+	public EnumLongOrderedMap (int initialCapacity) {
+		super(initialCapacity);
+	}
+
+	/**
+	 * Creates a new map with the specified initial capacity and load factor. This map will hold initialCapacity items before
+	 * growing the backing table.
+	 *
+	 * @param initialCapacity If not a power of two, it is increased to the next nearest power of two.
+	 * @param loadFactor      what fraction of the capacity can be filled before this has to resize; 0 &lt; loadFactor &lt;= 1
+	 */
+	public EnumLongOrderedMap (int initialCapacity, float loadFactor) {
+		super(initialCapacity, loadFactor);
+	}
+
+	/**
+	 * Creates a new map identical to the specified map.
+	 *
+	 * @param map an ObjectLongOrderedMap to copy, or a subclass such as this one
+	 */
+	public EnumLongOrderedMap (ObjectLongOrderedMap<? extends K> map) {
+		super(map);
+	}
+
+	/**
+	 * Creates a new map identical to the specified map.
+	 *
+	 * @param map an ObjectLongMap to copy, or a subclass
+	 */
+	public EnumLongOrderedMap (ObjectLongMap<? extends K> map) {
+		super(map);
+	}
+
+	/**
+	 * Given two side-by-side arrays, one of keys, one of values, this constructs a map and inserts each pair of key and value into it.
+	 * If keys and values have different lengths, this only uses the length of the smaller array.
+	 *
+	 * @param keys   an array of keys
+	 * @param values an array of values
+	 */
+	public EnumLongOrderedMap (K[] keys, long[] values) {
+		super(keys, values);
+	}
+
+	/**
+	 * Given two side-by-side collections, one of keys, one of values, this constructs a map and inserts each pair of key and value into it.
+	 * If keys and values have different lengths, this only uses the length of the smaller collection.
+	 *
+	 * @param keys   a Collection of keys
+	 * @param values a Collection of values
+	 */
+	public EnumLongOrderedMap (Collection<? extends K> keys, PrimitiveCollection.OfLong values) {
+		super(keys, values);
+	}
+
+	@Override
+	protected int place (Object item) {
+		// As long as the capacity is sufficient, ordinals will never collide.
+		if (item instanceof Enum)
+			return ((Enum<?>)item).ordinal() & mask;
+		return super.place(item);
+	}
+
+	@Override
+	protected boolean equate (Object left, @Nullable Object right) {
+		// Enums can use reference equality.
+		return left == right;
+	}
+
+	@Override
+	public int hashCode () {
+		int h = size;
+		K[] keyTable = this.keyTable;
+		long[] valueTable = this.valueTable;
+		for (int i = 0, n = keyTable.length; i < n; i++) {
+			K key = keyTable[i];
+			if (key != null) {
+				h += key.ordinal() * 421;
+				long value = valueTable[i];
+				h += value ^ value >>> 32;
+			}
+		}
+		return h;
+	}
+
+	@SuppressWarnings({"rawtypes"})
+	@Override
+	public boolean equals (Object obj) {
+		if (obj == this) {return true;}
+		if (!(obj instanceof EnumLongOrderedMap)) {return false;}
+		EnumLongOrderedMap other = (EnumLongOrderedMap)obj;
+		if (other.size != size) {return false;}
+		Enum[] keyTable = this.keyTable;
+		long[] valueTable = this.valueTable;
+		for (int i = 0, n = keyTable.length; i < n; i++) {
+			Enum key = keyTable[i];
+			if (key != null) {
+				long value = valueTable[i];
+				if (value != other.get(key)) {return false;}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Constructs a single-entry map given one key and one value.
+	 * This is mostly useful as an optimization for {@link #with(Enum, Number, Object...)}
+	 * when there's no "rest" of the keys or values.
+	 *
+	 * @param key0   the first and only key; must be an enum
+	 * @param value0 the first and only value
+	 * @return a new map containing just the entry mapping key0 to value0
+	 */
+	public static <K extends Enum<K>> EnumLongOrderedMap<K> with (K key0, Number value0) {
+		EnumLongOrderedMap<K> map = new EnumLongOrderedMap<>(1);
+		map.put(key0, value0.longValue());
+		return map;
+	}
+
+	/**
+	 * Constructs a map given alternating keys and values.
+	 * This can be useful in some code-generation scenarios, or when you want to make a
+	 * map conveniently by-hand and have it populated at the start. You can also use
+	 * {@link #EnumLongOrderedMap(Enum[], long[])}, which takes all keys and then all values.
+	 * This needs all keys to be {@code enum}s and all values to be {@code Number}s. Any keys that
+	 * aren't Enums or values that aren't Numbers have that entry skipped.
+	 *
+	 * @param key0   the first key; will be used to determine the type of all keys
+	 * @param value0 the first value; will be used to determine the type of all values
+	 * @param rest   an array or varargs of alternating K, V, K, V... elements
+	 * @param <V>    the type of values, inferred from value0
+	 * @return a new map containing the given keys and values
+	 */
+	@SuppressWarnings("unchecked")
+	public static <K extends Enum<K>, V> EnumLongOrderedMap<K> with (K key0, Number value0, Object... rest) {
+		EnumLongOrderedMap<K> map = new EnumLongOrderedMap<>(1 + (rest.length >>> 1));
+		map.put(key0, value0.longValue());
+		for (int i = 1; i < rest.length; i += 2) {
+			try {
+				map.put((K)rest[i - 1], ((Number)rest[i]).longValue());
+			} catch (ClassCastException ignored) {
+			}
+		}
+		return map;
+	}
+}
