9516d14cb2ab040da1dec3804b2edc0e37ed2424
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-12-07 15:01:22 -0800

    Start switching to CharFilter...

--- src/main/java/com/github/tommyettinger/ds/FilteredStringSet.java
+++ src/main/java/com/github/tommyettinger/ds/FilteredStringSet.java
@@ -26,7 +26,7 @@ import java.util.Collection;
  * A customizable variant on ObjectSet that always uses String keys, but only considers any character in an item (for
  * equality and hashing purposes) if that character satisfies a predicate. This can also edit the characters that pass
  * the filter, such as by changing their case during comparisons (and hashing). You will usually want to call
- * {@link #setFilter(CharPredicate)} and/or {@link #setEditor(CharToCharFunction)} to change the behavior of hashing and
+ * {@link #setFilter(CharFilter)} to change the behavior of hashing and
  * equality before you enter any items, unless you have specified the filter and/or editor you want in the constructor.
  * <br>
  * You can use this class as a replacement for {@link CaseInsensitiveSet} if you set the editor to a method reference to
@@ -42,8 +42,8 @@ import java.util.Collection;
  * reference to {@code Category.L::contains} acts like {@code Character::isLetter}, but works on GWT.
  */
 public class FilteredStringSet extends ObjectSet<String> {
-	protected CharPredicate 		filter = c -> true;
-	protected CharToCharFunction 	editor = c -> c;
+	protected CharFilter filter = CharFilter.getOrCreate("Identity", c -> true, c -> c);
+
 	/**
 	 * Creates a new set with an initial capacity of 51 and a load factor of {@link Utilities#getDefaultLoadFactor()}.
 	 * This considers all characters in a String key and does not edit them.
@@ -79,13 +79,11 @@ public class FilteredStringSet extends ObjectSet<String> {
 	 * Creates a new set with an initial capacity of 51 and a load factor of {@link Utilities#getDefaultLoadFactor()}.
 	 * This uses the specified filter and editor.
 	 *
-	 * @param filter a CharPredicate that should return true iff a character should be considered for equality/hashing
-	 * @param editor a CharToCharFunction that will take a char from a key String and return a potentially different char
+	 * @param filter a CharFilter that can be obtained with {@link CharFilter#getOrCreate(String, CharPredicate, CharToCharFunction)}
 	 */
-	public FilteredStringSet (CharPredicate filter, CharToCharFunction editor) {
+	public FilteredStringSet (CharFilter filter) {
 		super();
 		this.filter = filter;
-		this.editor = editor;
 	}
 
 	/**
@@ -93,15 +91,13 @@ public class FilteredStringSet extends ObjectSet<String> {
 	 * growing the backing table.
 	 * This uses the specified filter and editor.
 	 *
-	 * @param filter a CharPredicate that should return true iff a character should be considered for equality/hashing
-	 * @param editor a CharToCharFunction that will take a char from a key String and return a potentially different char
+	 * @param filter a CharFilter that can be obtained with {@link CharFilter#getOrCreate(String, CharPredicate, CharToCharFunction)}
 	 * @param initialCapacity If not a power of two, it is increased to the next nearest power of two.
 	 * @param loadFactor      what fraction of the capacity can be filled before this has to resize; 0 &lt; loadFactor &lt;= 1
 	 */
-	public FilteredStringSet (CharPredicate filter, CharToCharFunction editor, int initialCapacity, float loadFactor) {
+	public FilteredStringSet (CharFilter filter, int initialCapacity, float loadFactor) {
 		super(initialCapacity, loadFactor);
 		this.filter = filter;
-		this.editor = editor;
 	}
 
 	/**
@@ -112,19 +108,17 @@ public class FilteredStringSet extends ObjectSet<String> {
 	public FilteredStringSet (FilteredStringSet set) {
 		super(set);
 		filter = set.filter;
-		editor = set.editor;
 	}
 
 	/**
 	 * Creates a new set that contains all distinct elements in {@code coll}.
 	 * This uses the specified filter and editor, including while it enters the items in coll.
 	 *
-	 * @param filter a CharPredicate that should return true iff a character should be considered for equality/hashing
-	 * @param editor a CharToCharFunction that will take a char from a key String and return a potentially different char
+	 * @param filter a CharFilter that can be obtained with {@link CharFilter#getOrCreate(String, CharPredicate, CharToCharFunction)}
 	 * @param coll a Collection implementation to copy, such as an ObjectList or a Set that isn't a FilteredStringSet
 	 */
-	public FilteredStringSet (CharPredicate filter, CharToCharFunction editor, Collection<? extends String> coll) {
-		this(filter, editor, coll.size(), Utilities.getDefaultLoadFactor());
+	public FilteredStringSet (CharFilter filter, Collection<? extends String> coll) {
+		this(filter, coll.size(), Utilities.getDefaultLoadFactor());
 		addAll(coll);
 	}
 
@@ -132,14 +126,13 @@ public class FilteredStringSet extends ObjectSet<String> {
 	 * Creates a new set using {@code length} items from the given {@code array}, starting at {@code} offset (inclusive).
 	 * This uses the specified filter and editor, including while it enters the items in array.
 	 *
-	 * @param filter a CharPredicate that should return true iff a character should be considered for equality/hashing
-	 * @param editor a CharToCharFunction that will take a char from a key String and return a potentially different char
+	 * @param filter a CharFilter that can be obtained with {@link CharFilter#getOrCreate(String, CharPredicate, CharToCharFunction)}
 	 * @param array  an array to draw items from
 	 * @param offset the first index in array to draw an item from
 	 * @param length how many items to take from array; bounds-checking is the responsibility of the using code
 	 */
-	public FilteredStringSet (CharPredicate filter, CharToCharFunction editor, String[] array, int offset, int length) {
-		this(filter, editor, length, Utilities.getDefaultLoadFactor());
+	public FilteredStringSet (CharFilter filter, String[] array, int offset, int length) {
+		this(filter, length, Utilities.getDefaultLoadFactor());
 		addAll(array, offset, length);
 	}
 
@@ -147,76 +140,38 @@ public class FilteredStringSet extends ObjectSet<String> {
 	 * Creates a new set containing all the items in the given array.
 	 * This uses the specified filter and editor, including while it enters the items in array.
 	 *
-	 * @param filter a CharPredicate that should return true iff a character should be considered for equality/hashing
-	 * @param editor a CharToCharFunction that will take a char from a key String and return a potentially different char
+	 * @param filter a CharFilter that can be obtained with {@link CharFilter#getOrCreate(String, CharPredicate, CharToCharFunction)}
 	 * @param array an array that will be used in full, except for duplicate items
 	 */
-	public FilteredStringSet (CharPredicate filter, CharToCharFunction editor, String[] array) {
-		this(filter, editor, array, 0, array.length);
+	public FilteredStringSet (CharFilter filter, String[] array) {
+		this(filter, array, 0, array.length);
 	}
 
-	public CharPredicate getFilter() {
+	public CharFilter getFilter() {
 		return filter;
 	}
 
 	/**
-	 * Sets the filter that determines which characters in a String are considered for equality and hashing, then
-	 * returns this object, for chaining. Common CharPredicate filters you might use could be method references to
-	 * {@link Character#isLetter(char)} or {@link CharList#contains(char)}, for example. If the filter returns true for
-	 * a given character, that character will be used for hashing/equality; otherwise it will be ignored.
-	 * The default filter always returns true. If the filter changes, that invalidates anything previously entered into
+	 * Sets the CharFilter that determines which characters in a String are considered for equality and hashing, as well
+	 * as any changes made to characters before hashing or equating, then
+	 * returns this object, for chaining. If the filter changes, that invalidates anything previously entered into
 	 * this, so before changing the filter <em>this clears the entire data structure</em>, removing all existing items.
-	 * @param filter a CharPredicate that should return true iff a character should be considered for equality/hashing
+	 * @param filter a CharFilter that can be obtained with {@link CharFilter#getOrCreate(String, CharPredicate, CharToCharFunction)}
 	 * @return this, for chaining
 	 */
-	public FilteredStringSet setFilter(CharPredicate filter) {
+	public FilteredStringSet setFilter(CharFilter filter) {
 		clear();
 		this.filter = filter;
 		return this;
 	}
 
-	public CharToCharFunction getEditor() {
-		return editor;
-	}
-
-	/**
-	 * Sets the editor that can alter the characters in a String when they are being used for equality and hashing. This
-	 * does not apply any changes to the Strings in this data structure; it only affects how they are hashed or
-	 * compared. Common CharToCharFunction editors you might use could be a method reference to
-	 * {@link Character#toUpperCase(char)} (useful for case-insensitivity) or a lambda that could do... anything.
-	 * The default filter returns the char it is passed without changes. If the editor changes, that invalidates
-	 * anything previously entered into this, so before changing the editor <em>this clears the entire data
-	 * structure</em>, removing all existing items.
-	 * @param editor a CharToCharFunction that will take a char from a key String and return a potentially different char
-	 * @return this, for chaining
-	 */
-	public FilteredStringSet setEditor(CharToCharFunction editor) {
-		clear();
-		this.editor = editor;
-		return this;
-	}
-
-	/**
-	 * Equivalent to calling {@code mySet.setFilter(filter).setEditor(editor)}, but only clears the data structure once.
-	 * @see #setFilter(CharPredicate)
-	 * @see #setEditor(CharToCharFunction)
-	 * @param filter a CharPredicate that should return true iff a character should be considered for equality/hashing
-	 * @param editor a CharToCharFunction that will take a char from a key String and return a potentially different char
-	 * @return this, for chaining
-	 */
-	public FilteredStringSet setModifiers(CharPredicate filter, CharToCharFunction editor) {
-		clear();
-		this.filter = filter;
-		this.editor = editor;
-		return this;
-	}
 
 	protected long hashHelper(String s) {
 		long hash = 0x9E3779B97F4A7C15L + hashMultiplier; // golden ratio
 		for (int i = 0, len = s.length(); i < len; i++) {
 			char c = s.charAt(i);
-			if(filter.test(c)){
-				hash = (hash + editor.applyAsChar(c)) * hashMultiplier;
+			if(filter.filter.test(c)){
+				hash = (hash + filter.editor.applyAsChar(c)) * hashMultiplier;
 			}
 		}
 		return hash;
@@ -234,7 +189,7 @@ public class FilteredStringSet extends ObjectSet<String> {
 	 * Compares two objects for equality by the rules this filtered data structure uses for keys.
 	 * This will return true if the arguments are reference-equivalent or both null. Otherwise, it
 	 * requires that both are {@link String}s and compares them using the {@link #getFilter() filter}
-	 * and {@link #getEditor() editor} of this object.
+	 * of this object.
 	 *
 	 * @param left  must be non-null; typically a key being compared, but not necessarily
 	 * @param right may be null; typically a key being compared, but can often be null for an empty key slot, or some other type
@@ -254,17 +209,17 @@ public class FilteredStringSet extends ObjectSet<String> {
 			while (i < llen || j < rlen) {
 				if(i == llen) cl = -1;
 				else {
-					while (i < llen && !filter.test((char) (cl = l.charAt(i++)))) {
+					while (i < llen && !filter.filter.test((char) (cl = l.charAt(i++)))) {
 						cl = -1;
 					}
 				}
 				if(j == rlen) cr = -1;
 				else {
-					while (j < rlen && !filter.test((char) (cr = r.charAt(j++)))) {
+					while (j < rlen && !filter.filter.test((char) (cr = r.charAt(j++)))) {
 						cr = -1;
 					}
 				}
-				if(cl != cr && editor.applyAsChar((char)cl) != editor.applyAsChar((char)cr))
+				if(cl != cr && filter.editor.applyAsChar((char)cl) != filter.editor.applyAsChar((char)cr))
 					return false;
 			}
 			return true;
@@ -283,14 +238,14 @@ public class FilteredStringSet extends ObjectSet<String> {
 		return h;
 	}
 
-	public static FilteredStringSet with (CharPredicate filter, CharToCharFunction editor, String item) {
-		FilteredStringSet set = new FilteredStringSet(filter, editor, 1, Utilities.getDefaultLoadFactor());
+	public static FilteredStringSet with (CharFilter filter, String item) {
+		FilteredStringSet set = new FilteredStringSet(filter, 1, Utilities.getDefaultLoadFactor());
 		set.add(item);
 		return set;
 	}
 
-	public static FilteredStringSet with (CharPredicate filter, CharToCharFunction editor, String... array) {
-        return new FilteredStringSet(filter, editor, array);
+	public static FilteredStringSet with (CharFilter filter, String... array) {
+        return new FilteredStringSet(filter, array);
 	}
 
 }
