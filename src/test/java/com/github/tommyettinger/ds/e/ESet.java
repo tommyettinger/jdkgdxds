6c2df1bee0b9f6db356f655a3155bd13e9fc7bcd
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-03-26 20:44:42 -0700

    Partial implementation of ESet, in tests.
    This is an alternate version of EnumSet that should be closer to the java.util.EnumSet implementation while still permitting serialization with fairly normal tools.

---
+++ src/test/java/com/github/tommyettinger/ds/e/ESet.java
@@ -0,0 +1,311 @@
+/*
+ * Copyright (c) 2024 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.ds.e;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.AbstractSet;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * A Set of Enum items. Unlike {@link java.util.EnumSet}, this does not require a Class at construction time, which can be
+ * useful for serialization purposes.
+ */
+public class ESet extends AbstractSet<Enum<?>> implements Set<Enum<?>>, Iterable<Enum<?>> {
+	protected int size;
+	protected int[] table;
+	protected Enum<?>[] enumValues;
+
+	/**
+	 * Empty constructor; using this will postpone allocating any internal arrays until {@link #add(Enum)} is first called
+	 * (potentially indirectly).
+	 */
+	public ESet () {
+		super();
+	}
+
+	/**
+	 * Initializes this set so that it has exactly enough capacity as needed to contain each Enum constant defined in
+	 * {@code valuesResult}, assuming valuesResult stores every possible constant in one Enum type. As the name suggests,
+	 * you almost always would obtain valuesResult from calling {@code values()} on an Enum type, but you can share one
+	 * reference to one Enum array across many ESet instances if you don't modify the shared array.
+	 * @param valuesResult almost always, the result of calling {@code values()} on an Enum type; used directly, not copied
+	 */
+	public ESet (Enum<?>[] valuesResult) {
+		super();
+		if(valuesResult == null) return;
+		enumValues = valuesResult;
+		table = new int[valuesResult.length + 31 >>> 5];
+	}
+
+
+
+	/**
+	 * Returns the number of elements in this set (its cardinality).  If this
+	 * set contains more than {@code Integer.MAX_VALUE} elements, returns
+	 * {@code Integer.MAX_VALUE}.
+	 *
+	 * @return the number of elements in this set (its cardinality)
+	 */
+	@Override
+	public int size () {
+		return size;
+	}
+
+	/**
+	 * Returns {@code true} if this set contains no elements.
+	 *
+	 * @return {@code true} if this set contains no elements
+	 */
+	@Override
+	public boolean isEmpty () {
+		return size == 0;
+	}
+
+	/**
+	 * Returns {@code true} if this set contains the specified element.
+	 * More formally, returns {@code true} if and only if this set
+	 * contains an element {@code e} such that
+	 * {@code Objects.equals(item, e)}.
+	 *
+	 * @param item element whose presence in this set is to be tested
+	 * @return {@code true} if this set contains the specified element
+	 * @throws ClassCastException   if the type of the specified element
+	 *                              is incompatible with this set
+	 *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+	 * @throws NullPointerException if the specified element is null and this
+	 *                              set does not permit null elements
+	 *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+	 */
+	@Override
+	public boolean contains (Object item) {
+		if(table == null || item == null || size == 0 || !(item instanceof Enum<?>)) return false;
+		final Enum<?> e = (Enum<?>)item;
+		final int ord = e.ordinal(), upper = ord >>> 5;
+		if(table.length <= upper) return false;
+		// (1 << ord) has ord implicitly used modulo 32
+		return (table[upper] & 1 << ord) != 0;
+	}
+
+	/**
+	 * Returns an iterator over the elements in this set.  The elements are
+	 * returned in the order of their {@link Enum#ordinal()} values.
+	 *
+	 * @return an iterator over the elements in this set
+	 */
+	@Override
+	public @NonNull Iterator<Enum<?>> iterator () {
+		// TODO: iterator!
+		return null;
+	}
+
+	/**
+	 * Adds the specified element to this set if it is not already present
+	 * (optional operation).  More formally, adds the specified element
+	 * {@code e} to this set if the set contains no element {@code e2}
+	 * such that
+	 * {@code Objects.equals(e, e2)}.
+	 * If this set already contains the element, the call leaves the set
+	 * unchanged and returns {@code false}.  In combination with the
+	 * restriction on constructors, this ensures that sets never contain
+	 * duplicate elements.
+	 *
+	 * <p>The stipulation above does not imply that sets must accept all
+	 * elements; sets may refuse to add any particular element, including
+	 * {@code null}, and throw an exception, as described in the
+	 * specification for {@link Collection#add Collection.add}.
+	 * Individual set implementations should clearly document any
+	 * restrictions on the elements that they may contain.
+	 *
+	 * @param item element to be added to this set
+	 * @return {@code true} if this set did not already contain the specified
+	 * element
+	 * @throws UnsupportedOperationException if the {@code add} operation
+	 *                                       is not supported by this set
+	 * @throws ClassCastException            if the class of the specified element
+	 *                                       prevents it from being added to this set
+	 * @throws NullPointerException          if the specified element is null and this
+	 *                                       set does not permit null elements
+	 * @throws IllegalArgumentException      if some property of the specified element
+	 *                                       prevents it from being added to this set
+	 */
+	@Override
+	public boolean add (Enum<?> item) {
+		if(item == null) return false;
+		if(enumValues == null) enumValues = item.getDeclaringClass().getEnumConstants();
+		if(table == null) table = new int[enumValues.length + 31 >>> 5];
+		final int ord = item.ordinal(), upper = ord >>> 5;
+		if(table.length <= upper) return false;
+		// (1 << ord) has ord implicitly used modulo 32
+		boolean changed = (table[upper]) != (table[upper] |= 1 << ord);
+		if(changed) size++;
+		return changed;
+	}
+
+	/**
+	 * Removes the specified element from this set if it is present
+	 * (optional operation).  More formally, removes an element {@code e}
+	 * such that
+	 * {@code Objects.equals(item, e)}, if
+	 * this set contains such an element.  Returns {@code true} if this set
+	 * contained the element (or equivalently, if this set changed as a
+	 * result of the call).  (This set will not contain the element once the
+	 * call returns.)
+	 *
+	 * @param item object to be removed from this set, if present
+	 * @return {@code true} if this set contained the specified element
+	 * @throws ClassCastException            if the type of the specified element
+	 *                                       is incompatible with this set
+	 *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
+	 * @throws NullPointerException          if the specified element is null and this
+	 *                                       set does not permit null elements
+	 *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
+	 * @throws UnsupportedOperationException if the {@code remove} operation
+	 *                                       is not supported by this set
+	 */
+	@Override
+	public boolean remove (Object item) {
+		if(table == null || item == null || size == 0 || !(item instanceof Enum<?>)) return false;
+		final Enum<?> e = (Enum<?>)item;
+		final int ord = e.ordinal(), upper = ord >>> 5;
+		if(table.length <= upper) return false;
+		// (1 << ord) has ord implicitly used modulo 32
+		final boolean changed = (table[upper]) != (table[upper] &= ~(1 << ord));
+		if(changed) size--;
+		return changed;
+	}
+
+	/**
+	 * Returns {@code true} if this set contains all the elements of the
+	 * specified collection.  If the specified collection is also a set, this
+	 * method returns {@code true} if it is a <i>subset</i> of this set.
+	 *
+	 * @param c collection to be checked for containment in this set
+	 * @return {@code true} if this set contains all the elements of the
+	 * specified collection
+	 * @throws ClassCastException   if the types of one or more elements
+	 *                              in the specified collection are incompatible with this
+	 *                              set
+	 *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+	 * @throws NullPointerException if the specified collection contains one
+	 *                              or more null elements and this set does not permit null
+	 *                              elements
+	 *                              (<a href="Collection.html#optional-restrictions">optional</a>),
+	 *                              or if the specified collection is null
+	 * @see #contains(Object)
+	 */
+	@Override
+	public boolean containsAll (Collection<?> c) {
+		return false;
+	}
+
+	/**
+	 * Adds all the elements in the specified collection to this set if
+	 * they're not already present (optional operation).  If the specified
+	 * collection is also a set, the {@code addAll} operation effectively
+	 * modifies this set so that its value is the <i>union</i> of the two
+	 * sets.  The behavior of this operation is undefined if the specified
+	 * collection is modified while the operation is in progress.
+	 *
+	 * @param c collection containing elements to be added to this set
+	 * @return {@code true} if this set changed as a result of the call
+	 * @throws UnsupportedOperationException if the {@code addAll} operation
+	 *                                       is not supported by this set
+	 * @throws ClassCastException            if the class of an element of the
+	 *                                       specified collection prevents it from being added to this set
+	 * @throws NullPointerException          if the specified collection contains one
+	 *                                       or more null elements and this set does not permit null
+	 *                                       elements, or if the specified collection is null
+	 * @throws IllegalArgumentException      if some property of an element of the
+	 *                                       specified collection prevents it from being added to this set
+	 * @see #add(Enum)
+	 */
+	@Override
+	public boolean addAll (Collection<? extends Enum<?>> c) {
+		return false;
+	}
+
+	/**
+	 * Retains only the elements in this set that are contained in the
+	 * specified collection (optional operation).  In other words, removes
+	 * from this set all of its elements that are not contained in the
+	 * specified collection.  If the specified collection is also a set, this
+	 * operation effectively modifies this set so that its value is the
+	 * <i>intersection</i> of the two sets.
+	 *
+	 * @param c collection containing elements to be retained in this set
+	 * @return {@code true} if this set changed as a result of the call
+	 * @throws UnsupportedOperationException if the {@code retainAll} operation
+	 *                                       is not supported by this set
+	 * @throws ClassCastException            if the class of an element of this set
+	 *                                       is incompatible with the specified collection
+	 *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
+	 * @throws NullPointerException          if this set contains a null element and the
+	 *                                       specified collection does not permit null elements
+	 *                                       (<a href="Collection.html#optional-restrictions">optional</a>),
+	 *                                       or if the specified collection is null
+	 * @see #remove(Object)
+	 */
+	@Override
+	public boolean retainAll (Collection<?> c) {
+		return false;
+	}
+
+	/**
+	 * Removes from this set all of its elements that are contained in the
+	 * specified collection (optional operation).  If the specified
+	 * collection is also a set, this operation effectively modifies this
+	 * set so that its value is the <i>asymmetric set difference</i> of
+	 * the two sets.
+	 *
+	 * @param c collection containing elements to be removed from this set
+	 * @return {@code true} if this set changed as a result of the call
+	 * @throws UnsupportedOperationException if the {@code removeAll} operation
+	 *                                       is not supported by this set
+	 * @throws ClassCastException            if the class of an element of this set
+	 *                                       is incompatible with the specified collection
+	 *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
+	 * @throws NullPointerException          if this set contains a null element and the
+	 *                                       specified collection does not permit null elements
+	 *                                       (<a href="Collection.html#optional-restrictions">optional</a>),
+	 *                                       or if the specified collection is null
+	 * @see #remove(Object)
+	 * @see #contains(Object)
+	 */
+	@Override
+	public boolean removeAll (Collection<?> c) {
+		return false;
+	}
+
+	/**
+	 * Removes all the elements from this set (optional operation).
+	 * The set will be empty after this call returns.
+	 *
+	 * @throws UnsupportedOperationException if the {@code clear} method
+	 *                                       is not supported by this set
+	 */
+	@Override
+	public void clear () {
+		size = 0;
+		if(table != null)
+			Arrays.fill(table, 0);
+	}
+}
