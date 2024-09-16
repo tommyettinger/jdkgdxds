c08a0b9a87f38964390d8bb7b4c00e71848e3992
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-09-15 20:01:15 -0700

    AlteringIterator (generics); Gradle 8.10.1 .

--- src/main/java/com/github/tommyettinger/ds/support/iterator/AlteringIterator.java
+++ src/main/java/com/github/tommyettinger/ds/support/iterator/AlteringIterator.java
@@ -1,33 +1,34 @@
 package com.github.tommyettinger.ds.support.iterator;
 
-import com.github.tommyettinger.function.ObjToSameFunction;
+import com.github.tommyettinger.function.ObjToObjFunction;
+import com.github.tommyettinger.function.ObjToObjFunction;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
  * Wraps an Iterator so that it calls a function on each item it would otherwise return, and returns that function's
- * result.
- * If you need to return a different type than what the iterator returns, you can use an {@link AlteringIterator}.
+ * result. That means this is just like {@link EditingIterator}, except this may return a different type than what the
+ * iterator returns.
  * This has undefined behavior if any items the Iterator could return are modified during iteration.
  * <br>
- * You can change both the Iterator and editor at once using {@link #set(Iterator, ObjToSameFunction)}, and can also
+ * You can change both the Iterator and editor at once using {@link #set(Iterator, ObjToObjFunction)}, and can also
  * just change the Iterator with {@link #set(Iterator)}.
 
  * @param <T> the type of items this can return, and the type the wrapped Iterator returns
  */
-public class EditingIterator<T> implements Iterator<T> {
+public class AlteringIterator<T, R> implements Iterator<R> {
     public Iterator<T> iterator;
-    public ObjToSameFunction<T> editor;
+    public ObjToObjFunction<T, R> editor;
 
-    public EditingIterator() {
+    public AlteringIterator() {
     }
 
-    public EditingIterator(final Iterator<T> iterator, final ObjToSameFunction<T> editor) {
+    public AlteringIterator(final Iterator<T> iterator, final ObjToObjFunction<T, R> editor) {
         set(iterator, editor);
     }
 
-    public void set (final Iterator<T> iterator, final ObjToSameFunction<T> editor) {
+    public void set (final Iterator<T> iterator, final ObjToObjFunction<T, R> editor) {
         this.iterator = iterator;
         this.editor = editor;
     }
@@ -42,7 +43,7 @@ public class EditingIterator<T> implements Iterator<T> {
     }
 
     @Override
-    public T next () {
+    public R next () {
         if (!hasNext()) throw new NoSuchElementException("No elements remaining.");
         return editor.apply(iterator.next());
     }
