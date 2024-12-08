78d2f7a7ef9ccfbfd1ecdfe748474df380f11cb0
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-12-07 19:27:42 -0800

    Make them Comparable for sorting.

--- src/test/java/com/github/tommyettinger/ds/Junction.java
+++ src/test/java/com/github/tommyettinger/ds/Junction.java
@@ -48,13 +48,50 @@ public class Junction<T extends Comparable<T>> implements Term<T> {
         return "(" + root + ")";
     }
 
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     *
+     * <p>The implementor must ensure {@link Integer#signum
+     * signum}{@code (x.compareTo(y)) == -signum(y.compareTo(x))} for
+     * all {@code x} and {@code y}.  (This implies that {@code
+     * x.compareTo(y)} must throw an exception if and only if {@code
+     * y.compareTo(x)} throws an exception.)
+     *
+     * <p>The implementor must also ensure that the relation is transitive:
+     * {@code (x.compareTo(y) > 0 && y.compareTo(z) > 0)} implies
+     * {@code x.compareTo(z) > 0}.
+     *
+     * <p>Finally, the implementor must ensure that {@code
+     * x.compareTo(y)==0} implies that {@code signum(x.compareTo(z))
+     * == signum(y.compareTo(z))}, for all {@code z}.
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     * @apiNote It is strongly recommended, but <i>not</i> strictly required that
+     * {@code (x.compareTo(y)==0) == (x.equals(y))}.  Generally speaking, any
+     * class that implements the {@code Comparable} interface and violates
+     * this condition should clearly indicate this fact.  The recommended
+     * language is "Note: this class has a natural ordering that is
+     * inconsistent with equals."
+     */
+    @Override
+    public int compareTo(Term<T> o) {
+        return root.compareTo(o);
+    }
+
     public static class Leaf<T extends Comparable<T>> implements Term<T>{
-        public final @Nullable T item;
+        public final T item;
 
         public Leaf() {
-            item = null;
+            item = (T)(new Object());
         }
-        public Leaf(@Nullable T item) {
+        public Leaf(T item) {
             this.item = item;
         }
 
@@ -75,7 +112,7 @@ public class Junction<T extends Comparable<T>> implements Term<T> {
 
         @Override
         public String toString() {
-            return item == null ? "''" : item.toString();
+            return item.toString();
         }
 
         @Override
@@ -89,6 +126,11 @@ public class Junction<T extends Comparable<T>> implements Term<T> {
         @Override
         public int hashCode() {
             return Objects.hashCode(item);
+        }
+
+        @Override
+        public int compareTo(Term<T> o) {
+            return o instanceof Leaf ? item.compareTo(((Leaf<T>)o).item) : Integer.signum(o.symbol() - symbol());
         }
     }
     public static class Any<T extends Comparable<T>> implements Term<T>{
@@ -146,6 +188,20 @@ public class Junction<T extends Comparable<T>> implements Term<T> {
         @Override
         public int hashCode() {
             return contents.hashCode();
+        }
+
+        @Override
+        public int compareTo(Term<T> o) {
+            if(o instanceof Any) {
+                Any<T> a = (Any<T>)o;
+                if(contents.size() != a.contents.size())
+                    return contents.size() - a.contents.size();
+                for (int i = 0; i < contents.size(); i++) {
+                    int comp = contents.get(i).compareTo(a.contents.get(i));
+                    if(comp != 0) return comp;
+                }
+            }
+            return Integer.signum(o.symbol() - symbol());
         }
     }
 }
--- src/test/java/com/github/tommyettinger/ds/Term.java
+++ src/test/java/com/github/tommyettinger/ds/Term.java
@@ -6,7 +6,7 @@ import java.util.Collection;
  * Mostly internal; describes part of a {@link Junction}.
  * @param <T> the type being joined in the Junction; must be Comparable
  */
-public interface Term<T extends Comparable<T>> {
+public interface Term<T extends Comparable<T>> extends Comparable<Term<T>> {
     boolean match(Collection<? extends T> seq);
 
     char symbol();
