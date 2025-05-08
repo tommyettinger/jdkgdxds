19694f4f030fb07cd5b7ff3ab4648ac6b6689af1
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-05-07 20:07:16 -0700

    IntFloatOrderedMap...

--- src/main/java/com/github/tommyettinger/ds/IntFloatOrderedMap.java
+++ src/main/java/com/github/tommyettinger/ds/IntFloatOrderedMap.java
@@ -66,9 +66,8 @@ public class IntFloatOrderedMap extends IntFloatMap implements Ordered.OfInt {
 	/**
 	 * Creates a new map with an initial capacity of {@link Utilities#getDefaultTableCapacity()} and a load factor of {@link Utilities#getDefaultLoadFactor()}.
 	 */
-	public IntFloatOrderedMap () {
-		super();
-		keys = new IntList();
+	public IntFloatOrderedMap (boolean useDequeOrder) {
+		this(Utilities.getDefaultTableCapacity(), useDequeOrder);
 	}
 
 	/**
@@ -76,9 +75,8 @@ public class IntFloatOrderedMap extends IntFloatMap implements Ordered.OfInt {
 	 *
 	 * @param initialCapacity If not a power of two, it is increased to the next nearest power of two.
 	 */
-	public IntFloatOrderedMap (int initialCapacity) {
-		super(initialCapacity);
-		keys = new IntList(initialCapacity);
+	public IntFloatOrderedMap (int initialCapacity, boolean useDequeOrder) {
+		this(initialCapacity, Utilities.getDefaultLoadFactor(), useDequeOrder);
 	}
 
 	/**
@@ -88,9 +86,10 @@ public class IntFloatOrderedMap extends IntFloatMap implements Ordered.OfInt {
 	 * @param initialCapacity If not a power of two, it is increased to the next nearest power of two.
 	 * @param loadFactor      what fraction of the capacity can be filled before this has to resize; 0 &lt; loadFactor &lt;= 1
 	 */
-	public IntFloatOrderedMap (int initialCapacity, float loadFactor) {
+	public IntFloatOrderedMap (int initialCapacity, float loadFactor, boolean useDequeOrder) {
 		super(initialCapacity, loadFactor);
-		keys = new IntList(initialCapacity);
+		if(useDequeOrder) keys = new IntDeque(initialCapacity);
+		else keys = new IntList(initialCapacity);
 	}
 
 	/**
@@ -100,7 +99,8 @@ public class IntFloatOrderedMap extends IntFloatMap implements Ordered.OfInt {
 	 */
 	public IntFloatOrderedMap (IntFloatOrderedMap map) {
 		super(map);
-		keys = new IntList(map.keys);
+		if(map.keys instanceof IntDeque) keys = new IntDeque((IntDeque) map.keys);
+		else keys = new IntList(map.keys);
 	}
 
 	/**
@@ -108,8 +108,8 @@ public class IntFloatOrderedMap extends IntFloatMap implements Ordered.OfInt {
 	 *
 	 * @param map the map to copy
 	 */
-	public IntFloatOrderedMap (IntFloatMap map) {
-		this(map.size());
+	public IntFloatOrderedMap (IntFloatMap map, boolean useDequeOrder) {
+		this(map.size(), useDequeOrder);
 		IntIterator it = map.keySet().iterator();
 		while (it.hasNext()) {
 			int k = it.nextInt();
@@ -124,8 +124,8 @@ public class IntFloatOrderedMap extends IntFloatMap implements Ordered.OfInt {
 	 * @param keys   an array of keys
 	 * @param values an array of values
 	 */
-	public IntFloatOrderedMap (int[] keys, float[] values) {
-		this(Math.min(keys.length, values.length));
+	public IntFloatOrderedMap (int[] keys, float[] values, boolean useDequeOrder) {
+		this(Math.min(keys.length, values.length), useDequeOrder);
 		putAll(keys, values);
 	}
 
@@ -136,22 +136,92 @@ public class IntFloatOrderedMap extends IntFloatMap implements Ordered.OfInt {
 	 * @param keys   a PrimitiveCollection of keys
 	 * @param values a PrimitiveCollection of values
 	 */
-	public IntFloatOrderedMap (PrimitiveCollection.OfInt keys, PrimitiveCollection.OfFloat values) {
-		this(Math.min(keys.size(), values.size()));
+	public IntFloatOrderedMap (PrimitiveCollection.OfInt keys, PrimitiveCollection.OfFloat values, boolean useDequeOrder) {
+		this(Math.min(keys.size(), values.size()), useDequeOrder);
 		putAll(keys, values);
 	}
 
 	/**
-	 * Creates a new set by copying {@code count} items from the given IntFloatOrderedMap, starting at {@code offset} in that Map,
+	 * Creates a new set by copying {@code count} items from the given LongObjectOrderedMap, starting at {@code offset} in that Map,
 	 * into this.
 	 *
-	 * @param other  another IntFloatOrderedMap
+	 * @param other  another LongObjectOrderedMap of the same type
+	 * @param offset the first index in other's ordering to draw an item from
+	 * @param count  how many items to copy from other
+	 */
+	public IntFloatOrderedMap (IntFloatOrderedMap other, int offset, int count, boolean useDequeOrder) {
+		this(count, useDequeOrder);
+		putAll(0, other, offset, count);
+	}
+
+	/**
+	 * Creates a new map with an initial capacity of {@link Utilities#getDefaultTableCapacity()} and a load factor of {@link Utilities#getDefaultLoadFactor()}.
+	 */
+	public IntFloatOrderedMap () {
+		this(false);
+	}
+
+	/**
+	 * Creates a new map with the given starting capacity and a load factor of {@link Utilities#getDefaultLoadFactor()}.
+	 *
+	 * @param initialCapacity If not a power of two, it is increased to the next nearest power of two.
+	 */
+	public IntFloatOrderedMap (int initialCapacity) {
+		this(initialCapacity, Utilities.getDefaultLoadFactor(), false);
+	}
+
+	/**
+	 * Creates a new map with the specified initial capacity and load factor. This map will hold initialCapacity items before
+	 * growing the backing table.
+	 *
+	 * @param initialCapacity If not a power of two, it is increased to the next nearest power of two.
+	 * @param loadFactor      what fraction of the capacity can be filled before this has to resize; 0 &lt; loadFactor &lt;= 1
+	 */
+	public IntFloatOrderedMap (int initialCapacity, float loadFactor) {
+		this(initialCapacity, loadFactor, false);
+	}
+
+	/**
+	 * Creates a new map identical to the specified map.
+	 *
+	 * @param map the map to copy
+	 */
+	public IntFloatOrderedMap (IntFloatMap map) {
+		this(map, false);
+	}
+
+	/**
+	 * Given two side-by-side arrays, one of keys, one of values, this constructs a map and inserts each pair of key and value into it.
+	 * If keys and values have different lengths, this only uses the length of the smaller array.
+	 *
+	 * @param keys   an array of keys
+	 * @param values an array of values
+	 */
+	public IntFloatOrderedMap (int[] keys, float[] values) {
+		this(keys, values, false);
+	}
+
+	/**
+	 * Given two side-by-side collections, one of keys, one of values, this constructs a map and inserts each pair of key and value into it.
+	 * If keys and values have different lengths, this only uses the length of the smaller collection.
+	 *
+	 * @param keys   a PrimitiveCollection of keys
+	 * @param values a PrimitiveCollection of values
+	 */
+	public IntFloatOrderedMap (PrimitiveCollection.OfInt keys, PrimitiveCollection.OfFloat values) {
+		this(keys, values, false);
+	}
+
+	/**
+	 * Creates a new set by copying {@code count} items from the given LongObjectOrderedMap, starting at {@code offset} in that Map,
+	 * into this.
+	 *
+	 * @param other  another LongObjectOrderedMap of the same type
 	 * @param offset the first index in other's ordering to draw an item from
 	 * @param count  how many items to copy from other
 	 */
 	public IntFloatOrderedMap (IntFloatOrderedMap other, int offset, int count) {
-		this(count);
-		putAll(0, other, offset, count);
+		this(other, offset, count, false);
 	}
 
 	@Override
