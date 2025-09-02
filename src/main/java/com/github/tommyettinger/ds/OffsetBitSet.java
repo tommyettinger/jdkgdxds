d7257684e93d3acf12058208d42b0f992662e6ec
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-09-01 20:22:16 -0700

    Can't forget OffsetBitSet.

--- src/main/java/com/github/tommyettinger/ds/OffsetBitSet.java
+++ src/main/java/com/github/tommyettinger/ds/OffsetBitSet.java
@@ -913,4 +913,47 @@ public class OffsetBitSet implements PrimitiveSet.OfInt {
 		s.addAll(indices);
 		return s;
 	}
+
+	/**
+	 * Calls {@link #parse(String, String, boolean)} with brackets set to false.
+	 * @param str a String that will be parsed in full
+	 * @param delimiter the delimiter between items in str
+	 * @return a new collection parsed from str
+	 */
+	public static OffsetBitSet parse(String str, String delimiter) {
+		return parse(str, delimiter, false);
+	}
+
+	/**
+	 * Creates a new collection and fills it by calling {@link #addLegible(String, String, int, int)} on either all of
+	 * {@code str} (if {@code brackets} is false) or {@code str} without its first and last chars (if {@code brackets}
+	 * is true). Each item is expected to be separated by {@code delimiter}.
+	 * @param str a String that will be parsed in full (depending on brackets)
+	 * @param delimiter the delimiter between items in str
+	 * @param brackets if true, the first and last chars in str will be ignored
+	 * @return a new collection parsed from str
+	 */
+	public static OffsetBitSet parse(String str, String delimiter, boolean brackets) {
+		OffsetBitSet c = new OffsetBitSet();
+		if(brackets)
+			c.addLegible(str, delimiter, 1, str.length() - 1);
+		else
+			c.addLegible(str, delimiter);
+		return c;
+	}
+
+	/**
+	 * Creates a new collection and fills it by calling {@link #addLegible(String, String, int, int)} with the given
+	 * four parameters as-is.
+	 * @param str a String that will have the given section parsed
+	 * @param delimiter the delimiter between items in str
+	 * @param offset the first position to parse in str, inclusive
+	 * @param length how many chars to parse, starting from offset
+	 * @return a new collection parsed from str
+	 */
+	public static OffsetBitSet parse(String str, String delimiter, int offset, int length) {
+		OffsetBitSet c = new OffsetBitSet();
+		c.addLegible(str, delimiter, offset, length);
+		return c;
+	}
 }
