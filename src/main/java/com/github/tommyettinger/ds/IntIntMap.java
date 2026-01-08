23e7303895180bd9c27457217ce2a44188934072
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-01-07 18:44:18 -0800

    IntInt(Ordered)Map. Entry.

--- src/main/java/com/github/tommyettinger/ds/IntIntMap.java
+++ src/main/java/com/github/tommyettinger/ds/IntIntMap.java
@@ -1180,8 +1180,7 @@ public class IntIntMap implements Iterable<IntIntMap.Entry> {
 	}
 
 	public static class EntryIterator extends MapIterator implements Iterable<Entry>, Iterator<Entry> {
-		protected Entry entry = new Entry();
-
+		
 		public EntryIterator(IntIntMap map) {
 			super(map);
 		}
@@ -1199,13 +1198,11 @@ public class IntIntMap implements Iterable<IntIntMap.Entry> {
 			if (!hasNext) {
 				throw new NoSuchElementException();
 			}
-			int[] keyTable = map.keyTable;
+			Entry entry;
 			if (nextIndex == INDEX_ZERO) {
-				entry.key = 0;
-				entry.value = map.zeroValue;
+				entry = new Entry(0, map.zeroValue);
 			} else {
-				entry.key = keyTable[nextIndex];
-				entry.value = map.valueTable[nextIndex];
+				entry = new Entry(map.keyTable[nextIndex], map.valueTable[nextIndex]);
 			}
 			currentIndex = nextIndex;
 			findNextIndex();
@@ -1302,25 +1299,24 @@ public class IntIntMap implements Iterable<IntIntMap.Entry> {
 		}
 
 		/**
-		 * Append the remaining items that this can iterate through into the given Map.
-		 * Does not change the position of this iterator. Note that a Map is not a Collection.
+		 * Append the remaining items that this can iterate through into the given IntIntMap.
+		 * Does not change the position of this iterator.
 		 *
-		 * @param coll any modifiable Map; may have items appended into it
+		 * @param map a modifiable IntIntMap; may have items appended into it
 		 * @return the given map
 		 */
-		public IntIntMap appendInto(IntIntMap coll) {
+		public IntIntMap appendInto(IntIntMap map) {
 			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
 			boolean hn = iter.hasNext;
 			while (iter.hasNext) {
-				iter.next();
-				coll.put(iter.entry.key, iter.entry.value);
+				map.put(iter.map.keyTable[iter.nextIndex], iter.map.valueTable[iter.nextIndex]);
+				iter.findNextIndex();
 			}
 			iter.currentIndex = currentIdx;
 			iter.nextIndex = nextIdx;
 			iter.hasNext = hn;
-			return coll;
+			return map;
 		}
-
 	}
 
 	public static class Values implements PrimitiveCollection.OfInt {
--- src/main/java/com/github/tommyettinger/ds/IntIntOrderedMap.java
+++ src/main/java/com/github/tommyettinger/ds/IntIntOrderedMap.java
@@ -743,8 +743,8 @@ public class IntIntOrderedMap extends IntIntMap implements Ordered.OfInt {
 						throw new NoSuchElementException();
 					}
 					currentIndex = nextIndex;
-					entry.key = keys.get(nextIndex);
-					entry.value = map.get(entry.key);
+					int k = keys.get(nextIndex);
+					Entry entry = new Entry(k, map.get(k));
 					nextIndex++;
 					hasNext = nextIndex < map.size;
 					return entry;
@@ -755,13 +755,27 @@ public class IntIntOrderedMap extends IntIntMap implements Ordered.OfInt {
 					if (currentIndex < 0) {
 						throw new IllegalStateException("next must be called before remove.");
 					}
-					map.remove(entry.key);
+					map.remove(keys.get(currentIndex));
 					nextIndex--;
 					currentIndex = -1;
 				}
 			};
 		}
 
+		@Override
+		public IntIntMap appendInto(IntIntMap map) {
+			int currentIdx = iter.currentIndex, nextIdx = iter.nextIndex;
+			boolean hn = iter.hasNext;
+			while (iter.hasNext) {
+				int k = keys.get(iter.nextIndex);
+				map.put(k, iter.map.get(k));
+				iter.findNextIndex();
+			}
+			iter.currentIndex = currentIdx;
+			iter.nextIndex = nextIdx;
+			iter.hasNext = hn;
+			return map;
+		}
 	}
 
 	public static class OrderedMapKeys extends Keys {
