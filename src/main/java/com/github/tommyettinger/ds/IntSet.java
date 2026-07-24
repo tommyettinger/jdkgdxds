35741540c6a97257d7db3565ec8ae5a1b713bdf9
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-07-23 20:08:27 -0700

    IntSet...

--- src/main/java/com/github/tommyettinger/ds/IntSet.java
+++ src/main/java/com/github/tommyettinger/ds/IntSet.java
@@ -80,6 +80,15 @@ public class IntSet implements PrimitiveSet.SetOfInt {
 	protected int mask;
 
 	/**
+	 * Used by {@link #place} to mix hashCode() results.
+	 * This isn't actually used as a multiplier in most place() implementations, but as a kind of hash seed.
+	 * It usually changes whenever {@link #resize(int)} is called.
+	 * This only needs to be serialized if the full key and value tables are serialized, or if the iteration order should be
+	 * the same before and after serialization.
+	 */
+	protected int hashMultiplier;
+
+	/**
 	 * Creates a new set with an initial capacity of {@link Utilities#getDefaultTableCapacity()} and a load factor of {@link Utilities#getDefaultLoadFactor()}.
 	 */
 	public IntSet() {
@@ -112,6 +121,7 @@ public class IntSet implements PrimitiveSet.SetOfInt {
 		mask = tableSize - 1;
 		threshold = Math.min((int) (tableSize * (double) loadFactor + 1), mask);
 		shift = BitConversion.countLeadingZeros(mask) + 32;
+		hashMultiplier = Utilities.HASH_MULTIPLIERS[64 - shift];
 
 		keyTable = new int[tableSize];
 	}
@@ -131,6 +141,7 @@ public class IntSet implements PrimitiveSet.SetOfInt {
 	 */
 	public IntSet(IntSet set) {
 		this((int) (set.keyTable.length * set.loadFactor), set.loadFactor);
+		hashMultiplier = set.hashMultiplier;
 		System.arraycopy(set.keyTable, 0, keyTable, 0, set.keyTable.length);
 		size = set.size;
 		hasZeroValue = set.hasZeroValue;
@@ -175,7 +186,7 @@ public class IntSet implements PrimitiveSet.SetOfInt {
 	 * @return an index between 0 and {@link #mask} (both inclusive)
 	 */
 	protected int place(int item) {
-		return BitConversion.imul(item ^ mask, 0xFAB9E45B) >>> shift;
+		return BitConversion.imul(item ^ hashMultiplier, 0xFAB9E45B) >>> shift;
 	}
 
 	/**
@@ -402,6 +413,7 @@ public class IntSet implements PrimitiveSet.SetOfInt {
 		mask = newSize - 1;
 		threshold = Math.min((int) (newSize * (double) loadFactor + 1), mask);
 		shift = BitConversion.countLeadingZeros(mask) + 32;
+		hashMultiplier = Utilities.HASH_MULTIPLIERS[64 - shift];
 
 		int[] oldKeyTable = keyTable;
 
@@ -418,21 +430,24 @@ public class IntSet implements PrimitiveSet.SetOfInt {
 	}
 
 	/**
-	 * Effectively does nothing here because the hashMultiplier is not used currently.
+	 * The "hashMultiplier" is really used like a hash seed to modify how {@link #place} mixes hash codes.
+	 * It changes every time the capacity does, when the table resizes.
 	 *
-	 * @return 1; a hashMultiplier is not used in this class
+	 * @return the hash seed currently used at this capacity
 	 */
 	public int getHashMultiplier() {
-		return 1;
+		return hashMultiplier;
 	}
 
 	/**
-	 * Effectively does nothing here because the hashMultiplier is not used currently.
-	 * Subclasses can use this to set some kind of identifier or user data, though.
+	 * Sets the "hashMultiplier" if and only if this has size 0.
+	 * The "hashMultiplier" is really used like a hash seed to modify how {@link #place} mixes hash codes.
+	 * Subclasses can also use this to set some kind of identifier or user data if they don't use a seed in place().
 	 *
-	 * @param hashMultiplier any int; will not be used
+	 * @param hashMultiplier any int to use as a hash seed; this does not have any constraints in this implementation
 	 */
 	public void setHashMultiplier(int hashMultiplier) {
+		if(size == 0) this.hashMultiplier = hashMultiplier;
 	}
 
 	public float getLoadFactor() {
